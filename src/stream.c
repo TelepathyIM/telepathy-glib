adbf4b6240bfb9a91be4b620fdd1eacebb6823bf
Senko Rasic <senko.rasic@collabora.co.uk>
2007-03-12 11:58:21 +0000

    stream.c: pass relay-token to transmitter object if possible
    20070312115821-01440-ddd6d2a8a666a92f74748f6ce65604d92d08680e.gz

--- src/stream.c
+++ src/stream.c
@@ -818,6 +818,14 @@ set_stream_properties (TpStreamEngineStream *self,
     {
       DEBUG (self, "setting farsight transmitter to %s", transmitter);
       g_object_set (stream, "transmitter", transmitter, NULL);
+
+      {
+        GObject *xmit = NULL;
+        g_object_get (priv->fs_stream, "transmitter-object", &xmit, NULL);
+        if ((props->relay_token != NULL) && g_object_has_property (xmit, "relay-token"))
+            g_object_set (xmit, "relay-token", props->relay_token, NULL);
+      }
+
     }
 
   if (props->stun_server != NULL)
