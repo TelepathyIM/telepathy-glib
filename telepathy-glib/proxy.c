b34772567035cdd698815c133ff96fcac8ccdaeb
Simon McVittie <simon.mcvittie@collabora.co.uk>
2007-11-14 14:37:24 +0000

    Add TpProxy, a subclass of DBusGProxy that will eventually know about GetInterfaces()
    20071114143724-53eee-5ed57397214c7696554769bb2c3ff31718517eb1.gz

---
+++ telepathy-glib/proxy.c
@@ -0,0 +1,61 @@
+/*
+ * proxy.c - Base class for Telepathy client proxies
+ *
+ * Copyright (C) 2007 Collabora Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "telepathy-glib/proxy.h"
+
+#include <telepathy-glib/errors.h>
+
+struct _TpProxyClass {
+    DBusGProxyClass parent_class;
+    /*<private>*/
+};
+
+struct _TpProxy {
+    DBusGProxy parent;
+    /*<private>*/
+};
+
+G_DEFINE_TYPE (TpProxy,
+    tp_proxy,
+    DBUS_TYPE_G_PROXY);
+
+DBusGProxy *
+tp_proxy_get_interface (TpProxy *self,
+                        GQuark interface,
+                        GError **error)
+{
+  /* stub implementation - for now we claim no object has any interfaces */
+  g_set_error (error, TP_ERRORS, TP_ERROR_NOT_IMPLEMENTED,
+      "Object %s does not have interface %s",
+      dbus_g_proxy_get_path ((DBusGProxy *) self),
+      g_quark_to_string (interface));
+
+  return NULL;
+}
+
+static void
+tp_proxy_init (TpProxy *self)
+{
+}
+
+static void
+tp_proxy_class_init (TpProxyClass *klass)
+{
+}
---
+++ telepathy-glib/proxy.h
@@ -0,0 +1,56 @@
+/*
+ * proxy.h - Base class for Telepathy client proxies
+ *
+ * Copyright (C) 2007 Collabora Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef __TP_PROXY_H__
+#define __TP_PROXY_H__
+
+#include <dbus/dbus-glib.h>
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+typedef struct _TpProxy TpProxy;
+typedef struct _TpProxyClass TpProxyClass;
+
+GType tp_proxy_get_type (void);
+
+/* TYPE MACROS */
+#define TP_TYPE_PROXY \
+  (tp_proxy_get_type ())
+#define TP_PROXY(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj), TP_TYPE_PROXY, \
+                              TpProxy))
+#define TP_PROXY_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST((klass), TP_TYPE_PROXY, \
+                           TpProxyClass))
+#define TP_IS_PROXY(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj), TP_TYPE_PROXY))
+#define TP_IS_PROXY_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE((klass), TP_TYPE_PROXY))
+#define TP_PROXY_GET_CLASS(obj) \
+  (G_TYPE_INSTANCE_GET_CLASS ((obj), TP_TYPE_PROXY, \
+                              TpProxyClass))
+
+DBusGProxy *tp_proxy_get_interface (TpProxy *self, GQuark interface,
+    GError **error);
+
+G_END_DECLS
+
+#endif /* #ifndef __TP_PROXY_H__*/
