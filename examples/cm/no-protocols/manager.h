5ea42d8191b7b1d76ee03e4ecd46db025db8276e
Simon McVittie <simon.mcvittie@collabora.co.uk>
2007-11-15 13:27:04 +0000

    Change no-protocols example CM to have an all-permissive license
    20071115132704-53eee-e5d69c95957e64fb7ed1f690ef074617f4b76abc.gz

--- examples/cm/no-protocols/manager.h
+++ examples/cm/no-protocols/manager.h
@@ -2,19 +2,9 @@
  * manager.h - header for an example connection manager
  * Copyright (C) 2007 Collabora Ltd.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * Copying and distribution of this file, with or without modification,
+ * are permitted in any medium without royalty provided the copyright
+ * notice and this notice are preserved.
  */
 
 #ifndef __EXAMPLE_CONNECTION_MANAGER_H__
