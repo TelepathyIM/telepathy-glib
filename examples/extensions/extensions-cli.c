a6560ba407f074b5fb8b10a0eb8bb65500d5a729
Simon McVittie <simon.mcvittie@collabora.co.uk>
2008-01-14 14:16:04 +0000

    Set EXAMPLE_IFACE_QUARK as prefix for interface quark generation in extension example
    20080114141604-53eee-0d66367abf40ae985c1053eb4425babc1ea77826.gz

--- examples/extensions/extensions-cli.c
+++ examples/extensions/extensions-cli.c
@@ -5,10 +5,6 @@
 
 #include "_gen/signals-marshal.h"
 
-/* FIXME: evil hack */
-#define TP_IFACE_QUARK_CONNECTION_INTERFACE_HATS \
-  EXAMPLE_IFACE_QUARK_CONNECTION_INTERFACE_HATS
-
 /* include auto-generated stubs for client-specific code */
 #include "_gen/cli-connection-body.h"
 #include "_gen/register-dbus-glib-marshallers-body.h"
