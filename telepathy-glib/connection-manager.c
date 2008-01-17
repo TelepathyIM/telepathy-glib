7d5327efa136b0dc2807eccc1fca792069715858
Simon McVittie <simon.mcvittie@collabora.co.uk>
2008-01-17 11:20:42 +0000

    connection-manager: copyright 2008
    20080117112042-53eee-d97f94017c7bc70932d92bc7879f69808be86167.gz

--- telepathy-glib/connection-manager.c
+++ telepathy-glib/connection-manager.c
@@ -1,8 +1,8 @@
 /*
  * connection-manager.c - proxy for a Telepathy connection manager
  *
- * Copyright (C) 2007 Collabora Ltd. <http://www.collabora.co.uk/>
- * Copyright (C) 2007 Nokia Corporation
+ * Copyright (C) 2007-2008 Collabora Ltd. <http://www.collabora.co.uk/>
+ * Copyright (C) 2007-2008 Nokia Corporation
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
