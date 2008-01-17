2b466550ad3faa69b121e27b55d5aa1167fabd1f
Simon McVittie <simon.mcvittie@collabora.co.uk>
2008-01-17 11:22:47 +0000

    glib-client-gen: make *_run_* output a GMainLoop, not a TpPendingCall
    20080117112247-53eee-5878d40cf7314a364c90e7071f71229869a7585c.gz

--- tools/glib-client-gen.py
+++ tools/glib-client-gen.py
@@ -765,7 +765,7 @@ class Generator(object):
         #       const GArray *in_properties,
         #       GPtrArray **out0,
         #       GError **error,
-        #       TpProxyPendingCall **pending_call);
+        #       GMainLoop **loop);
 
         self.b('typedef struct {')
         self.b('    GMainLoop *loop;')
@@ -867,10 +867,10 @@ class Generator(object):
 
         self.b(' * @error: If not %NULL, used to return errors if %FALSE ')
         self.b(' *  is returned')
-        self.b(' * @pending_call: If not %NULL, set before re-entering ')
-        self.b(' *  the main loop, to point to a #TpProxyPendingCall ')
+        self.b(' * @loop: If not %NULL, set before re-entering ')
+        self.b(' *  the main loop, to point to a #GMainLoop ')
         self.b(' *  which can be used to cancel this call with ')
-        self.b(' *  tp_proxy_pending_call_cancel(), causing a return of ')
+        self.b(' *  g_main_loop_quit(), causing a return of ')
         self.b(' *  %FALSE with @error set to %TP_DBUS_ERROR_CANCELLED')
         self.b(' *')
         self.b(' * Call the method %s and run the main loop' % member)
@@ -905,11 +905,11 @@ class Generator(object):
             self.b('    %s*%s,' % (ctype, name))
 
         self.h('    GError **error,')
-        self.h('    TpProxyPendingCall **pending_call);')
+        self.h('    GMainLoop **loop);')
         self.h('')
 
         self.b('    GError **error,')
-        self.b('    TpProxyPendingCall **pending_call)')
+        self.b('    GMainLoop **loop)')
         self.b('{')
         self.b('  DBusGProxy *iface;')
         self.b('  GQuark interface = %s;' % self.get_iface_quark())
@@ -941,8 +941,8 @@ class Generator(object):
         self.b('      %s,' % reentrant_invoke)
         self.b('      NULL, &state, NULL, NULL, TRUE);')
         self.b('')
-        self.b('  if (pending_call != NULL)')
-        self.b('    *pending_call = pc;')
+        self.b('  if (loop != NULL)')
+        self.b('    *loop = state.loop;')
         self.b('')
         self.b('  tp_proxy_pending_call_v0_take_pending_call (pc,')
         self.b('      dbus_g_proxy_begin_call_with_timeout (iface,')
@@ -964,6 +964,13 @@ class Generator(object):
         self.b('')
         self.b('  if (!state.completed)')
         self.b('    g_main_loop_run (state.loop);')
+        self.b('')
+        self.b('  if (!state.completed)')
+        self.b('    tp_proxy_pending_call_cancel (pc);')
+        self.b('')
+        self.b('  if (loop != NULL)')
+        self.b('    *loop = NULL;')
+        self.b('')
         self.b('  g_main_loop_unref (state.loop);')
         self.b('')
         self.b('  return state.success;')
