8a8100f8e3085b73f748f80dde57dfae9800d3e3
Simon McVittie <simon.mcvittie@collabora.co.uk>
2008-02-12 10:14:32 +0000

    Include dbus-daemon.xml in stable-interfaces.xml so we generate its iface quark code automatically
    20080212101432-53eee-68192d781f54c325d1e21ef24727db9a41b76639.gz

--- telepathy-glib/interfaces.c
+++ telepathy-glib/interfaces.c
@@ -1,28 +1,4 @@
 #include <telepathy-glib/interfaces.h>
 
-#include <dbus/dbus-shared.h>
-
-/* This one isn't auto-generated */
-
-/**
- * TP_IFACE_QUARK_DBUS_DAEMON:
- *
- * Expands to a call to a function that returns a quark whose string value is
- * %DBUS_INTERFACE_DBUS, the main interface exported by the D-Bus daemon
- *
- * Since: 0.7.1
- */
-GQuark
-tp_iface_quark_dbus_daemon (void)
-{
-  static GQuark quark = 0;
-
-  if (G_UNLIKELY (quark == 0))
-    {
-      quark = g_quark_from_static_string (DBUS_INTERFACE_DBUS);
-    }
-  return quark;
-}
-
 /* auto-generated implementation stubs */
 #include "_gen/interfaces-body.h"
--- telepathy-glib/interfaces.h
+++ telepathy-glib/interfaces.h
@@ -7,10 +7,6 @@ G_BEGIN_DECLS
 
 #include <telepathy-glib/_gen/telepathy-interfaces.h>
 
-/* This one isn't auto-generated */
-#define TP_IFACE_QUARK_DBUS_DAEMON (tp_iface_quark_dbus_daemon ())
-GQuark tp_iface_quark_dbus_daemon (void);
-
 G_END_DECLS
 
 #endif
