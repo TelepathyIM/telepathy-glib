e54ac3f0ea0b58ee2c26b2803c8054041068b782
Olivier Crete <olivier.crete@collabora.co.uk>
2008-04-10 15:00:18 +0000

    Ignore unknown cname errors
    20080410150018-3e2dc-35c2b80024ba9f2e205f69cdd9b1657bacfd55e8.gz

--- src/stream.c
+++ src/stream.c
@@ -1338,6 +1338,13 @@ tp_stream_engine_stream_bus_message (TpStreamEngineStream *stream,
           msg = gst_structure_get_string (s, "error-msg");
           debug = gst_structure_get_string (s, "debug-msg");
 
+          /*
+           * We ignore the Unknown Cname error because current signalling
+           * does no provide us with a cname
+           */
+          if (errno == FS_ERROR_UNKNOWN_CNAME)
+            return TRUE;
+
 
           enumclass = g_type_class_ref (FS_TYPE_ERROR);
           enumvalue = g_enum_get_value (enumclass, errorno);
