e7a91cd809a3e3c24aa524700e43c59e13aac7fd
Sjoerd Simons <sjoerd@luon.net>
2008-06-16 13:11:38 +0000

    language cleanup
    20080616131138-93b9a-9ed46ccd2292aaa909eb7816f6ae471d281721d0.gz

--- telepathy-glib/presence-mixin.h
+++ telepathy-glib/presence-mixin.h
@@ -118,9 +118,9 @@ void tp_presence_status_free (TpPresenceStatus *status);
  * available to be set on the connection.
  *
  * When implementing the
- * org.freedesktop.Telepathy.Connection.Interface.SimplePresence interface can
- * be called while DISCONNECTED to determine which statuses can be set in that
- * state.
+ * org.freedesktop.Telepathy.Connection.Interface.SimplePresence interface 
+ * this can be called while DISCONNECTED to determine which statuses can be set
+ * in that state.
  *
  * Returns: %TRUE if the status is available, %FALSE if not.
  */
