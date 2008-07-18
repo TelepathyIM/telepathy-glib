ae9ce9f015dd6b94a3c859b2f5731827cb9f789d
Simon McVittie <simon.mcvittie@collabora.co.uk>
2008-07-18 18:15:40 +0000

    Correct comment in autogen.sh
    20080718181540-53eee-e195c5a4ca730aeaf347f3bebdbe1e99a8a108d5.gz

--- autogen.sh
+++ autogen.sh
@@ -7,7 +7,7 @@ if test -n "$AUTOMAKE"; then
     : # don't override an explicit user request
 elif automake-1.9 --version >/dev/null 2>/dev/null && \
      aclocal-1.9 --version >/dev/null 2>/dev/null; then
-    # If we have automake-1.8, use it. This helps to ensure that our build
+    # If we have automake-1.9, use it. This helps to ensure that our build
     # system doesn't accidentally grow automake-1.10 dependencies.
     AUTOMAKE=automake-1.9
     export AUTOMAKE
