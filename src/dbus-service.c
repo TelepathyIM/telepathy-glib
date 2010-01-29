24aebbfe6f09485cde13c45d48b34bfac3ab1f61
Cosimo Alfarano <cosimo.alfarano@collabora.co.uk>
2010-01-29 19:03:03 +0000

    TP Restyling

--- src/dbus-service.c
+++ src/dbus-service.c
@@ -34,11 +34,15 @@
 	(dbus_g_type_get_struct ("GValueArray", G_TYPE_STRING, G_TYPE_STRING, G_TYPE_UINT, G_TYPE_INVALID))
 
 
-static gboolean
-tpl_dbus_service_last_chats (TplDBusService *self,
-		gchar const* account_path, gchar const* identifier,
-		gboolean is_chatroom, guint lines, DBusGMethodInvocation *context);
+static gboolean tpl_dbus_service_last_chats (TplDBusService *self,
+    const gchar *account_path, const gchar *identifier, gboolean is_chatroom,
+    guint lines, DBusGMethodInvocation *context);
 
+#define GET_PRIV(obj) TPL_GET_PRIV (obj, TplDBusService)
+struct _TplDBusServicePriv
+{
+  TplLogManager *manager;
+};
 
 #include <dbus-service-server.h>
 
@@ -53,32 +57,35 @@ tpl_dbus_service_finalize (GObject *obj)
 static void
 tpl_dbus_service_dispose (GObject *obj)
 {
-	//TplDBusService *self = TPL_DBUS_SERVICE(obj);
-
-	g_debug("TplDBusService: disposing\n");
-
 	G_OBJECT_CLASS (tpl_dbus_service_parent_class)->finalize (obj);
-
-	g_debug("TplDBusService: disposed\n");
 }
 
 
 static void
-tpl_dbus_service_class_init(TplDBusServiceClass* klass)
+tpl_dbus_service_class_init (TplDBusServiceClass* klass)
 {
 	GObjectClass* object_class = G_OBJECT_CLASS (klass);
+
 	object_class->finalize = tpl_dbus_service_finalize;
 	object_class->dispose = tpl_dbus_service_dispose;
 
-	dbus_g_object_type_install_info (TPL_TYPE_DBUS_SERVICE,
-			&dbus_glib_tpl_dbus_service_object_info);
+  dbus_g_object_type_install_info (TPL_TYPE_DBUS_SERVICE,
+      &dbus_glib_tpl_dbus_service_object_info);
 
+  g_type_class_add_private (object_class, sizeof (TplDBusServicePriv));
 }
+
 
 static void
 tpl_dbus_service_init(TplDBusService* self)
 {
-	self->manager = tpl_log_manager_dup_singleton ();
+  TplDBusServicePriv *priv = G_TYPE_INSTANCE_GET_PRIVATE (self,
+      TPL_TYPE_DBUS_SERVICE, TplDBusServicePriv);
+
+  g_return_if_fail (TPL_IS_DBUS_SERVICE (self));
+
+  self->priv = priv;
+	GET_PRIV (self)->manager = tpl_log_manager_dup_singleton ();
 }
 
 
@@ -88,19 +95,6 @@ tpl_dbus_service_new (void)
 	return g_object_new(TPL_TYPE_DBUS_SERVICE, NULL);
 }
 
-/*
-static TplDBusServiceChatMessage *
-tpl_dbus_service_chat_message_new(void)
-{
-	return g_slice_alloc0 (sizeof (TplDBusServiceChatMessage));
-}
-
-void
-tpl_dbus_service_chat_message_free(TplDBusServiceChatMessage *data)
-{
-	g_free(data);
-}
-*/
 
 static gboolean
 _pack_last_chats_answer (GList *data,
@@ -112,37 +106,40 @@ _pack_last_chats_answer (GList *data,
 	(*array) = g_ptr_array_new_with_free_func ((GDestroyNotify) g_value_array_free);
 	retval = *array;
 
-	for(data_idx=0; data_idx<g_list_length(data); ++data_idx)
-	{
-		TplLogEntry *log = g_list_nth_data (data, data_idx);
+  for(data_idx=0; data_idx<g_list_length(data); ++data_idx)
+    {
+      TplLogEntry *log = g_list_nth_data (data, data_idx);
 
-		GValue *value = g_new0(GValue, 1);
+      GValue *value = g_new0(GValue, 1);
 
-		gchar *message = g_strdup (
-				tpl_log_entry_text_get_message (TPL_LOG_ENTRY_TEXT (log)));
-    gchar *sender = g_strdup (tpl_contact_get_identifier(
+      gchar *message = g_strdup (tpl_log_entry_text_get_message (
+          TPL_LOG_ENTRY_TEXT (log)));
+      gchar *sender = g_strdup (tpl_contact_get_identifier (
           tpl_log_entry_text_get_sender (TPL_LOG_ENTRY_TEXT (log))));
-		guint timestamp = tpl_log_entry_get_timestamp (log);
+      guint timestamp = tpl_log_entry_get_timestamp (log);
 
-		g_value_init(value, DBUS_STRUCT_STRING_STRING_UINT);
-		g_value_take_boxed (value, dbus_g_type_specialized_construct
-        (DBUS_STRUCT_STRING_STRING_UINT));
+      g_value_init (value, DBUS_STRUCT_STRING_STRING_UINT);
+      g_value_take_boxed (value, dbus_g_type_specialized_construct (
+          DBUS_STRUCT_STRING_STRING_UINT));
 
-    dbus_g_type_struct_set (value, 0, sender, 1, message, 2, timestamp,
-      G_MAXUINT);
-		g_ptr_array_add (retval, g_value_get_boxed (value));
-		g_free (value);
+      dbus_g_type_struct_set (value, 0, sender, 1, message, 2, timestamp,
+          G_MAXUINT);
+      g_ptr_array_add (retval, g_value_get_boxed (value));
+      g_free (value);
 
-		g_debug ("retval[%d]=\"[%d] <%s>: %s\"\n", data_idx,
-				timestamp, sender, message);
-	}
+      g_debug ("retval[%d]=\"[%d] <%s>: %s\"\n", data_idx,
+          timestamp, sender, message);
+    }
 	return TRUE;
 }
 
 static gboolean
 tpl_dbus_service_last_chats (TplDBusService *self,
-		gchar const* account_path, gchar const* identifier,
-		gboolean is_chatroom, guint lines, DBusGMethodInvocation *context)
+		const gchar *account_path,
+    const gchar *identifier,
+		gboolean is_chatroom,
+    guint lines,
+    DBusGMethodInvocation *context)
 {
 	guint dates_idx;
 	gint msgs_idx;
@@ -153,61 +150,63 @@ tpl_dbus_service_last_chats (TplDBusService *self,
 	GList *ret=NULL;
 	GPtrArray *answer;
 	guint left_lines = lines;
+  TplDBusServicePriv *priv = GET_PRIV (self);
 
-	g_return_val_if_fail(context!=NULL, FALSE);
+  g_return_val_if_fail (TPL_IS_DBUS_SERVICE (self), FALSE);
+	g_return_val_if_fail(context != NULL, FALSE);
 
 	dbus = tp_get_bus();
 	tp_dbus = tp_dbus_daemon_new(dbus);
 
 	account = tp_account_new(tp_dbus, account_path, &error);
-	if (error!=NULL) {
-		g_error("TpAccount creation: %s",
-				error->message);
-		dbus_g_method_return_error(context, error);
-		g_error_free(error);
-		g_object_unref(tp_dbus);
-		g_object_unref(dbus);
-		return FALSE;
-	}
+	if (error!=NULL)
+    {
+      g_error("TpAccount creation: %s", error->message);
+      dbus_g_method_return_error(context, error);
+      g_error_free(error);
+      g_object_unref(tp_dbus);
+      g_object_unref(dbus);
+      return FALSE;
+    }
 
-	GList *dates = tpl_log_manager_get_dates(self->manager, account,
-			identifier, is_chatroom);
-	if(!dates) {
-		g_set_error_literal(&error,
-				TPL_DBUS_SERVICE_ERROR,
-				TPL_DBUS_SERVICE_ERROR_FAILED,
-				"Error during date list retrieving");
-		dbus_g_method_return_error(context, error);
-		//g_error_free(error);
-		g_object_unref(tp_dbus);
-		g_object_unref(dbus);
-		return FALSE;
-	}
+  GList *dates = tpl_log_manager_get_dates(priv->manager, account, identifier,
+      is_chatroom);
+	if(dates != NULL)
+    {
+      g_set_error_literal(&error, TPL_DBUS_SERVICE_ERROR,
+          TPL_DBUS_SERVICE_ERROR_FAILED, "Error during date list retrieving");
+      dbus_g_method_return_error(context, error);
+      g_object_unref(tp_dbus);
+      g_object_unref(dbus);
+      return FALSE;
+    }
 	dates = g_list_reverse(dates);
 
-	for(dates_idx=0; dates_idx<g_list_length(dates) && left_lines>0; ++dates_idx) {
-		gchar *date = g_list_nth_data(dates, dates_idx);
-		GList *messages = tpl_log_manager_get_messages_for_date(self->manager,
-				account, identifier, is_chatroom, date);
-		guint msgs_len = g_list_length (messages);
-		gint guard = (msgs_len>=left_lines ? left_lines : msgs_len);
+	for(dates_idx=0; dates_idx<g_list_length(dates) && left_lines>0; ++dates_idx)
+    {
+      gchar *date = g_list_nth_data(dates, dates_idx);
+      GList *messages = tpl_log_manager_get_messages_for_date(priv->manager,
+          account, identifier, is_chatroom, date);
+      guint msgs_len = g_list_length (messages);
+      gint guard = (msgs_len>=left_lines ? left_lines : msgs_len);
 
-		for(msgs_idx=msgs_len-1; guard>0 && left_lines>0; --guard, --msgs_idx) {
-			TplLogEntry *log = g_list_nth_data(messages, msgs_idx);
-			g_object_ref(log);
-			ret = g_list_prepend(ret, log);
-			left_lines-=1;
-		}
-		g_list_foreach(messages, (GFunc) g_object_unref, NULL); // unref TplLogEntries
-	}
-	g_list_foreach(dates, (GFunc) g_free, NULL);
+      for(msgs_idx=msgs_len-1; guard>0 && left_lines>0; --guard, --msgs_idx)
+        {
+          TplLogEntry *log = g_list_nth_data(messages, msgs_idx);
+          g_object_ref(log);
+          ret = g_list_prepend(ret, log);
+          left_lines-=1;
+        }
+      g_list_foreach(messages, (GFunc) g_object_unref, NULL);
+    }
+  g_list_foreach(dates, (GFunc) g_free, NULL);
 
-	_pack_last_chats_answer(ret, &answer);
-	g_list_foreach(ret, (GFunc) g_object_unref, NULL);
+  _pack_last_chats_answer(ret, &answer);
+  g_list_foreach(ret, (GFunc) g_object_unref, NULL);
 
-	dbus_g_method_return(context, answer);
+  dbus_g_method_return(context, answer);
 
-	g_object_unref(tp_dbus);
+  g_object_unref(tp_dbus);
 
-	return TRUE;
+  return TRUE;
 }
--- src/dbus-service.h
+++ src/dbus-service.h
@@ -22,51 +22,56 @@
 #ifndef __TPL_DBUS_SERVICE_H__
 #define __TPL_DBUS_SERVICE_H__
 
+#include <glib-object.h>
+
+#include <telepathy-logger/log-manager.h>
+
 #define TPL_DBUS_SRV_WELL_KNOWN_BUS_NAME \
   "org.freedesktop.Telepathy.TelepathyLoggerService"
 #define TPL_DBUS_SRV_OBJECT_PATH \
   "/org/freedesktop/Telepathy/TelepathyLoggerService"
 
-#include <glib-object.h>
-
-#include <telepathy-logger/log-manager.h>
-
 G_BEGIN_DECLS
 
-#define TPL_TYPE_DBUS_SERVICE                  (tpl_dbus_service_get_type ())
-#define TPL_DBUS_SERVICE(obj)                  (G_TYPE_CHECK_INSTANCE_CAST ((obj), TPL_TYPE_DBUS_SERVICE, TplDBusService))
-#define TPL_DBUS_SERVICE_CLASS(klass)          (G_TYPE_CHECK_CLASS_CAST ((klass), TPL_TYPE_DBUS_SERVICE, TplDBusServiceClass))
-#define TPL_IS_DBUS_SERVICE(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), TPL_TYPE_DBUS_SERVICE))
+#define TPL_TYPE_DBUS_SERVICE            (tpl_dbus_service_get_type ())
+#define TPL_DBUS_SERVICE(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), TPL_TYPE_DBUS_SERVICE, TplDBusService))
+#define TPL_DBUS_SERVICE_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), TPL_TYPE_DBUS_SERVICE, TplDBusServiceClass))
+#define TPL_IS_DBUS_SERVICE(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), TPL_TYPE_DBUS_SERVICE))
 #define TPL_IS_DBUS_SERVICE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), TPL_TYPE_DBUS_SERVICE))
 #define TPL_DBUS_SERVICE_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), TPL_TYPE_DBUS_SERVICE, TplDBusServiceClass))
 
 #define TPL_DBUS_SERVICE_ERROR g_quark_from_static_string ("tpl-dbus-service-error-quark")
-typedef enum {
-	TPL_DBUS_SERVICE_ERROR_FAILED
+typedef enum
+{
+  TPL_DBUS_SERVICE_ERROR_FAILED
 } TplDBusServiceError;
 
-typedef struct {
-	GObject parent;
+typedef struct _TplDBusServicePriv TplDBusServicePriv;
+typedef struct
+{
+  GObject parent;
 
-	/* Private */
-	TplLogManager *manager;
+  /* Private */
+  TplDBusServicePriv *priv;
 } TplDBusService;
 
-typedef struct {
-	GObjectClass	parent_class;
+
+typedef struct
+{
+  GObjectClass	parent_class;
 } TplDBusServiceClass;
 
-typedef struct {
-	long unsigned	timestamp;
-	gchar *sender;
-	gchar *message;
+typedef struct
+{
+  long unsigned	timestamp;
+  gchar *sender;
+  gchar *message;
 } TplDBusServiceChatMessage;
 
 GType tpl_dbus_service_get_type (void);
 
 TplDBusService *tpl_dbus_service_new (void);
 
-
 G_END_DECLS
 
-#endif // __TPL_DBUS_SERVICE_H__
+#endif
