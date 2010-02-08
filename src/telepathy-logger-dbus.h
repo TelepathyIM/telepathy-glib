ce4bcf346f52c6fd9ad41108317a3207f1ed370c
Cosimo Alfarano <cosimo.alfarano@collabora.co.uk>
2010-02-08 17:15:08 +0000

    Merged the TPL and TPL DBus service
    * TPL and TPL-DBus are now running within the same daemon telepahty-logger

--- src/telepathy-logger-dbus.h
+++
@@ -1,5 +0,0 @@
-#ifndef __TELEPATHY_LOGGER_DBUS_H__
-
-void telepathy_logger_dbus_init (void);
-
-#endif // __TELEPATHY_LOGGER_DBUS_H__
--- src/telepathy-logger.c
+++ src/telepathy-logger.c
@@ -19,13 +19,45 @@
  * Authors: Cosimo Alfarano <cosimo.alfarano@collabora.co.uk>
  */
 
+#include "dbus-service.h"
+
 #include <glib.h>
+#include <telepathy-glib/dbus.h>
 
 #include <telepathy-logger/channel-factory.h>
 #include <telepathy-logger/channel-text.h>
 #include <telepathy-logger/observer.h>
 
 static GMainLoop *loop = NULL;
+
+
+static void
+telepathy_logger_dbus_init (void)
+{
+	TplDBusService *dbus_srv;
+	DBusGConnection *bus;
+	TpDBusDaemon *tp_bus;
+	GError *error = NULL;
+
+	bus = tp_get_bus();
+	tp_bus = tp_dbus_daemon_new(bus);
+
+	if ( tp_dbus_daemon_request_name (tp_bus, TPL_DBUS_SRV_WELL_KNOWN_BUS_NAME,
+			TRUE, &error) ) {
+		g_print("%s DBus well known name registered\n",
+				TPL_DBUS_SRV_WELL_KNOWN_BUS_NAME);
+	} else {
+		g_print("Well Known name request error: %s\n", error->message);
+		g_clear_error(&error);
+		g_error_free(error);
+	}
+
+	dbus_srv = tpl_dbus_service_new ();
+	dbus_g_connection_register_g_object (bus,
+			TPL_DBUS_SRV_OBJECT_PATH,
+			G_OBJECT(dbus_srv));
+}
+
 
 int
 main(int argc,
@@ -51,6 +83,8 @@ main(int argc,
       g_debug ("Error during D-Bus registration: %s", error->message);
       return 1;
     }
+
+  telepathy_logger_dbus_init ();
 
 	loop = g_main_loop_new (NULL, FALSE);
 	g_main_loop_run (loop);
