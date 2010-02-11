cc900e536d1fc4b1d9f8f98c3297e7007650b0b9
Danielle Madeley <danielle.madeley@collabora.co.uk>
2010-02-11 14:55:03 +1100

    Get org.freedesktop.Telepathy.Logger extension building

---
+++ extensions/extensions.h
@@ -0,0 +1,15 @@
+#ifndef _TPL_EXTENSIONS_H
+#define _TPL_EXTENSIONS_H
+
+#include "extensions/_gen/enums.h"
+#include "extensions/_gen/svc.h"
+
+G_BEGIN_DECLS
+
+#include "extensions/_gen/gtypes.h"
+#include "extensions/_gen/interfaces.h"
+
+G_END_DECLS
+
+#endif /* _GABBLE_EXTENSIONS_H */
+
