db0d236feae0b878d975f6231737affcad245ebe
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2010-04-15 12:15:56 +0200

    create_and_connect_conn: use g_assert rather than MYASSERT

--- tests/dbus/contacts.c
+++ tests/dbus/contacts.c
@@ -1037,19 +1037,19 @@ create_and_connect_conn (GType conn_type,
         "protocol", "simple",
         NULL));
   service_conn_as_base = TP_BASE_CONNECTION (*service_conn);
-  MYASSERT (*service_conn != NULL, "");
-  MYASSERT (service_conn_as_base != NULL, "");
+  g_assert (*service_conn != NULL);
+  g_assert (service_conn_as_base != NULL);
 
-  MYASSERT (tp_base_connection_register (service_conn_as_base, "simple",
-        &name, &conn_path, &error), "");
+  g_assert (tp_base_connection_register (service_conn_as_base, "simple",
+        &name, &conn_path, &error));
   test_assert_no_error (error);
 
   *client_conn = tp_connection_new (dbus, name, conn_path,
       &error);
-  MYASSERT (*client_conn != NULL, "");
+  g_assert (*client_conn != NULL);
   test_assert_no_error (error);
-  MYASSERT (tp_connection_run_until_ready (*client_conn, TRUE, &error,
-        NULL), "");
+  g_assert (tp_connection_run_until_ready (*client_conn, TRUE, &error,
+        NULL));
   test_assert_no_error (error);
 
   g_free (name);
