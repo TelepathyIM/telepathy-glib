5fdd04080214249bec5f395261fdb0b48bbc3aca
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2010-04-21 13:44:11 +0200

    test Observer properties

--- tests/dbus/base-client.c
+++ tests/dbus/base-client.c
@@ -78,6 +78,8 @@ teardown (Test *test,
     }
 }
 
+/* Test Basis */
+
 static void
 test_basis (Test *test,
     gconstpointer data G_GNUC_UNUSED)
@@ -99,6 +101,8 @@ test_basis (Test *test,
   g_object_unref (dbus);
   g_free (name);
 }
+
+/* Test register */
 
 static void
 get_client_prop_cb (TpProxy *proxy,
@@ -148,6 +152,102 @@ test_register (Test *test,
   g_assert_no_error (test->error);
 }
 
+/* Test Observer */
+static void
+check_filters (GPtrArray *filters)
+{
+  GHashTable *filter;
+
+  g_assert (filters != NULL);
+  g_assert_cmpuint (filters->len, ==, 2);
+
+  filter = g_ptr_array_index (filters, 0);
+  g_assert_cmpuint (g_hash_table_size (filter), ==, 1);
+  g_assert_cmpstr (tp_asv_get_string (filter, TP_PROP_CHANNEL_CHANNEL_TYPE), ==,
+      TP_IFACE_CHANNEL_TYPE_TEXT);
+
+  filter = g_ptr_array_index (filters, 1);
+  g_assert_cmpuint (g_hash_table_size (filter), ==, 2);
+  g_assert_cmpstr (tp_asv_get_string (filter, TP_PROP_CHANNEL_CHANNEL_TYPE), ==,
+      TP_IFACE_CHANNEL_TYPE_STREAM_TUBE);
+  g_assert_cmpuint (tp_asv_get_uint32 (filter,
+        TP_PROP_CHANNEL_TARGET_HANDLE_TYPE, NULL), ==, TP_HANDLE_TYPE_CONTACT);
+}
+
+static void
+get_observer_prop_cb (TpProxy *proxy,
+    GHashTable *properties,
+    const GError *error,
+    gpointer user_data,
+    GObject *weak_object)
+{
+  Test *test = user_data;
+  GPtrArray *filters;
+  gboolean recover;
+  gboolean valid;
+
+  if (error != NULL)
+    {
+      test->error = g_error_copy (error);
+      goto out;
+    }
+
+  g_assert_cmpint (g_hash_table_size (properties), == , 2);
+
+  filters = tp_asv_get_boxed (properties, "ObserverChannelFilter",
+      TP_ARRAY_TYPE_CHANNEL_CLASS_LIST);
+  check_filters (filters);
+
+  recover = tp_asv_get_boolean (properties, "Recover", &valid);
+  g_assert (valid);
+  g_assert (recover);
+
+out:
+  g_main_loop_quit (test->mainloop);
+}
+
+static void
+test_observer (Test *test,
+    gconstpointer data G_GNUC_UNUSED)
+{
+  GHashTable *filter;
+
+  filter = tp_asv_new (
+      TP_PROP_CHANNEL_CHANNEL_TYPE, G_TYPE_STRING, TP_IFACE_CHANNEL_TYPE_TEXT,
+      NULL);
+
+  tp_base_client_add_observer_filter (test->base_client, filter);
+  g_hash_table_unref (filter);
+
+  tp_base_client_take_observer_filter (test->base_client, tp_asv_new (
+        TP_PROP_CHANNEL_CHANNEL_TYPE, G_TYPE_STRING,
+          TP_IFACE_CHANNEL_TYPE_STREAM_TUBE,
+        TP_PROP_CHANNEL_TARGET_HANDLE_TYPE, G_TYPE_UINT,
+          TP_HANDLE_TYPE_CONTACT,
+        NULL));
+
+  tp_base_client_set_observer_recover (test->base_client, TRUE);
+
+  tp_base_client_register (test->base_client);
+
+  /* Check Client properties */
+  tp_cli_dbus_properties_call_get_all (test->client, -1,
+      TP_IFACE_CLIENT, get_client_prop_cb, test, NULL, NULL);
+  g_main_loop_run (test->mainloop);
+
+  g_assert_no_error (test->error);
+  g_assert_cmpint (g_strv_length (test->interfaces), ==, 1);
+  g_assert (tp_strv_contains ((const gchar * const *) test->interfaces,
+        TP_IFACE_CLIENT_OBSERVER));
+
+  /* Check Observer properties */
+  tp_cli_dbus_properties_call_get_all (test->client, -1,
+      TP_IFACE_CLIENT_OBSERVER, get_observer_prop_cb, test, NULL, NULL);
+  g_main_loop_run (test->mainloop);
+
+  g_assert_no_error (test->error);
+}
+
 int
 main (int argc,
       char **argv)
@@ -160,6 +260,8 @@ main (int argc,
 
   g_test_add ("/base-client/basis", Test, NULL, setup, test_basis, teardown);
   g_test_add ("/base-client/register", Test, NULL, setup, test_register,
+      teardown);
+  g_test_add ("/base-client/observer", Test, NULL, setup, test_observer,
       teardown);
 
   return g_test_run ();
