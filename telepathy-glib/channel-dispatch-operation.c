c3a4ec003596ca553e7b5f83e214a22afe27414b
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2010-04-29 12:47:26 +0200

    channel-dispatch-operation: add GObject properties

--- telepathy-glib/channel-dispatch-operation.c
+++ telepathy-glib/channel-dispatch-operation.c
@@ -21,10 +21,16 @@
 
 #include "telepathy-glib/channel-dispatch-operation.h"
 
+#include <telepathy-glib/account.h>
+#include <telepathy-glib/channel.h>
+#include <telepathy-glib/connection.h>
 #include <telepathy-glib/defs.h>
 #include <telepathy-glib/errors.h>
+#include <telepathy-glib/gtypes.h>
 #include <telepathy-glib/interfaces.h>
+#include <telepathy-glib/proxy-internal.h>
 #include <telepathy-glib/proxy-subclass.h>
+#include <telepathy-glib/util.h>
 
 #define DEBUG_FLAG TP_DEBUG_DISPATCHER
 #include "telepathy-glib/dbus-internal.h"
@@ -104,7 +110,21 @@
  */
 
 struct _TpChannelDispatchOperationPrivate {
-    gpointer dummy;
+  TpConnection *connection;
+  TpAccount *account;
+  GPtrArray *channels;
+  GStrv possible_handlers;
+  GHashTable *immutable_properties;
+};
+
+enum
+{
+  PROP_CONNECTION = 1,
+  PROP_ACCOUNT,
+  PROP_CHANNELS,
+  PROP_POSSIBLE_HANDLERS,
+  PROP_CHANNEL_DISPATCH_OPERATION_PROPERTIES,
+  N_PROPS
 };
 
 G_DEFINE_TYPE (TpChannelDispatchOperation, tp_channel_dispatch_operation,
@@ -115,6 +135,9 @@ tp_channel_dispatch_operation_init (TpChannelDispatchOperation *self)
 {
   self->priv = G_TYPE_INSTANCE_GET_PRIVATE (self,
       TP_TYPE_CHANNEL_DISPATCH_OPERATION, TpChannelDispatchOperationPrivate);
+
+  self->priv->immutable_properties = g_hash_table_new_full (g_str_hash,
+      g_str_equal, g_free, (GDestroyNotify) tp_g_value_slice_free);
 }
 
 static void
@@ -126,6 +149,219 @@ tp_channel_dispatch_operation_finished_cb (TpChannelDispatchOperation *self,
       "ChannelDispatchOperation finished and was removed" };
 
   tp_proxy_invalidate ((TpProxy *) self, &e);
+}
+
+static void
+tp_channel_dispatch_operation_get_property (GObject *object,
+    guint property_id,
+    GValue *value,
+    GParamSpec *pspec)
+{
+  TpChannelDispatchOperation *self = TP_CHANNEL_DISPATCH_OPERATION (object);
+
+  switch (property_id)
+    {
+    case PROP_CONNECTION:
+      g_value_set_object (value, self->priv->connection);
+      break;
+
+    case PROP_ACCOUNT:
+      g_value_set_object (value, self->priv->account);
+      break;
+
+    case PROP_CHANNELS:
+      g_value_set_boxed (value, self->priv->channels);
+      break;
+
+    case PROP_POSSIBLE_HANDLERS:
+      g_value_set_boxed (value, self->priv->possible_handlers);
+      break;
+
+    case PROP_CHANNEL_DISPATCH_OPERATION_PROPERTIES:
+      g_value_set_boxed (value, self->priv->immutable_properties);
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+      break;
+  }
+}
+
+static void
+maybe_set_connection (TpChannelDispatchOperation *self,
+    const gchar *path)
+{
+  TpDBusDaemon *dbus;
+  GError *error = NULL;
+
+  if (self->priv->connection != NULL)
+    return;
+
+  if (path == NULL)
+    return;
+
+  dbus = tp_proxy_get_dbus_daemon (self);
+
+  self->priv->connection = tp_connection_new (dbus, NULL, path, &error);
+  if (self->priv->connection == NULL)
+    {
+      DEBUG ("Failed to create connection %s: %s", path, error->message);
+      g_error_free (error);
+      return;
+    }
+
+  g_object_notify ((GObject *) self, "connection");
+
+  if (g_hash_table_lookup (self->priv->immutable_properties,
+        TP_PROP_CHANNEL_DISPATCH_OPERATION_CONNECTION) != NULL)
+    return;
+
+  g_hash_table_insert (self->priv->immutable_properties,
+      g_strdup (TP_PROP_CHANNEL_DISPATCH_OPERATION_CONNECTION),
+      tp_g_value_slice_new_boxed (DBUS_TYPE_G_OBJECT_PATH, path));
+}
+
+static void
+maybe_set_account (TpChannelDispatchOperation *self,
+    const gchar *path)
+{
+  TpDBusDaemon *dbus;
+  GError *error = NULL;
+
+  if (self->priv->account != NULL)
+    return;
+
+  if (path == NULL)
+    return;
+
+  dbus = tp_proxy_get_dbus_daemon (self);
+
+  self->priv->account = tp_account_new (dbus, path, &error);
+  if (self->priv->account == NULL)
+    {
+      DEBUG ("Failed to create account %s: %s", path, error->message);
+      g_error_free (error);
+      return;
+    }
+
+  g_object_notify ((GObject *) self, "account");
+
+  if (g_hash_table_lookup (self->priv->immutable_properties,
+        TP_PROP_CHANNEL_DISPATCH_OPERATION_ACCOUNT) != NULL)
+    return;
+
+  g_hash_table_insert (self->priv->immutable_properties,
+      g_strdup (TP_PROP_CHANNEL_DISPATCH_OPERATION_ACCOUNT),
+      tp_g_value_slice_new_boxed (DBUS_TYPE_G_OBJECT_PATH, path));
+}
+
+static void
+maybe_set_possible_handlers (TpChannelDispatchOperation *self,
+    GStrv handlers)
+{
+  if (self->priv->possible_handlers != NULL)
+    return;
+
+  if (handlers == NULL)
+    return;
+
+  self->priv->possible_handlers = g_strdupv (handlers);
+
+  g_object_notify ((GObject *) self, "possible-handlers");
+
+  if (g_hash_table_lookup (self->priv->immutable_properties,
+        TP_PROP_CHANNEL_DISPATCH_OPERATION_POSSIBLE_HANDLERS) != NULL)
+    return;
+
+  g_hash_table_insert (self->priv->immutable_properties,
+      g_strdup (TP_PROP_CHANNEL_DISPATCH_OPERATION_POSSIBLE_HANDLERS),
+      tp_g_value_slice_new_boxed (G_TYPE_STRV, handlers));
+}
+
+static void
+maybe_set_interfaces (TpChannelDispatchOperation *self,
+    const gchar **interfaces)
+{
+  const gchar **iter;
+
+  if (interfaces == NULL)
+    return;
+
+  for (iter = interfaces; *iter != NULL; iter++)
+    {
+      DEBUG ("- %s", *iter);
+
+      if (tp_dbus_check_valid_interface_name (*iter, NULL))
+        {
+          GQuark q = g_quark_from_string (*iter);
+          tp_proxy_add_interface_by_id ((TpProxy *) self, q);
+        }
+      else
+        {
+          DEBUG ("\tInterface %s not valid, ignoring it", *iter);
+        }
+    }
+
+  g_hash_table_insert (self->priv->immutable_properties,
+      g_strdup (TP_PROP_CHANNEL_DISPATCH_OPERATION_INTERFACES),
+      tp_g_value_slice_new_boxed (G_TYPE_STRV, interfaces));
+}
+
+static void
+tp_channel_dispatch_operation_set_property (GObject *object,
+    guint property_id,
+    const GValue *value,
+    GParamSpec *pspec)
+{
+  TpChannelDispatchOperation *self = TP_CHANNEL_DISPATCH_OPERATION (object);
+
+  switch (property_id)
+    {
+    case PROP_CONNECTION:
+      self->priv->connection = g_value_dup_object (value);
+      break;
+
+    case PROP_ACCOUNT:
+      self->priv->account = g_value_dup_object (value);
+      break;
+
+    case PROP_POSSIBLE_HANDLERS:
+      self->priv->possible_handlers = g_value_dup_boxed (value);
+      break;
+
+    case PROP_CHANNEL_DISPATCH_OPERATION_PROPERTIES:
+      {
+        GHashTable *asv = g_value_get_boxed (value);
+
+        if (asv == NULL)
+          return;
+
+        tp_g_hash_table_update (self->priv->immutable_properties,
+            asv, (GBoxedCopyFunc) g_strdup,
+            (GBoxedCopyFunc) tp_g_value_slice_dup);
+
+        maybe_set_connection (self, tp_asv_get_boxed (asv,
+              TP_PROP_CHANNEL_DISPATCH_OPERATION_CONNECTION,
+              DBUS_TYPE_G_OBJECT_PATH));
+
+        maybe_set_account (self, tp_asv_get_boxed (asv,
+              TP_PROP_CHANNEL_DISPATCH_OPERATION_ACCOUNT,
+              DBUS_TYPE_G_OBJECT_PATH));
+
+        maybe_set_possible_handlers (self, tp_asv_get_boxed (asv,
+              TP_PROP_CHANNEL_DISPATCH_OPERATION_POSSIBLE_HANDLERS,
+              G_TYPE_STRV));
+
+        maybe_set_interfaces (self, tp_asv_get_boxed (asv,
+              TP_PROP_CHANNEL_DISPATCH_OPERATION_INTERFACES,
+              G_TYPE_STRV));
+      }
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+      break;
+  }
 }
 
 static void
@@ -155,14 +391,157 @@ tp_channel_dispatch_operation_constructed (GObject *object)
 }
 
 static void
+tp_channel_dispatch_operation_dispose (GObject *object)
+{
+  TpChannelDispatchOperation *self = TP_CHANNEL_DISPATCH_OPERATION (object);
+  void (*dispose) (GObject *) =
+    G_OBJECT_CLASS (tp_channel_dispatch_operation_parent_class)->dispose;
+
+  if (self->priv->connection != NULL)
+    {
+      g_object_unref (self->priv->connection);
+      self->priv->connection = NULL;
+    }
+
+  if (self->priv->account != NULL)
+    {
+      g_object_unref (self->priv->account);
+      self->priv->account = NULL;
+    }
+
+  if (self->priv->channels != NULL)
+    {
+      g_ptr_array_foreach (self->priv->channels, (GFunc) g_object_unref, NULL);
+      g_ptr_array_free (self->priv->channels, TRUE);
+      self->priv->channels = NULL;
+    }
+
+  g_strfreev (self->priv->possible_handlers);
+  self->priv->possible_handlers = NULL;
+
+  if (self->priv->immutable_properties != NULL)
+    {
+      g_hash_table_unref (self->priv->immutable_properties);
+      self->priv->immutable_properties = NULL;
+    }
+
+  if (dispose != NULL)
+    dispose (object);
+}
+
+static void
 tp_channel_dispatch_operation_class_init (TpChannelDispatchOperationClass *klass)
 {
   TpProxyClass *proxy_class = (TpProxyClass *) klass;
   GObjectClass *object_class = (GObjectClass *) klass;
+  GParamSpec *param_spec;
 
   g_type_class_add_private (klass, sizeof (TpChannelDispatchOperationPrivate));
 
+  object_class->get_property = tp_channel_dispatch_operation_get_property;
+  object_class->set_property = tp_channel_dispatch_operation_set_property;
   object_class->constructed = tp_channel_dispatch_operation_constructed;
+  object_class->dispose = tp_channel_dispatch_operation_dispose;
+
+  /**
+   * TpChannelDispatchOperation:connection:
+   *
+   * The #TpConnection with which the channels are associated.
+   *
+   * Read-only except during construction.
+   *
+   * This is not guaranteed to be set until tp_proxy_prepare_async() has
+   * finished preparing %TP_CHANNEL_DISPATCH_OPERATION_FEATURE_CORE.
+   *
+   * Since: 0.11.UNRELEASED
+   */
+  param_spec = g_param_spec_object ("connection", "TpConnection",
+      "The TpConnection of this channel dispatch operation",
+      TP_TYPE_CONNECTION,
+      G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
+  g_object_class_install_property (object_class, PROP_CONNECTION,
+      param_spec);
+
+  /**
+   * TpChannelDispatchOperation:account:
+   *
+   * The #TpAccount with which the connection and channels are associated.
+   *
+   * Read-only except during construction.
+   *
+   * This is not guaranteed to be set until tp_proxy_prepare_async() has
+   * finished preparing %TP_CHANNEL_DISPATCH_OPERATION_FEATURE_CORE.
+   *
+   * Since: 0.11.UNRELEASED
+   */
+  param_spec = g_param_spec_object ("account", "TpAccount",
+      "The TpAccount of this channel dispatch operation",
+      TP_TYPE_ACCOUNT,
+      G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
+  g_object_class_install_property (object_class, PROP_ACCOUNT,
+      param_spec);
+
+  /**
+   * TpChannelDispatchOperation:channels:
+   *
+   * A #GPtrArray containing the #TpChannel to be dispatched.
+   *
+   * Read-only.
+   *
+   * This is not guaranteed to be set until tp_proxy_prepare_async() has
+   * finished preparing %TP_CHANNEL_DISPATCH_OPERATION_FEATURE_CORE.
+   *
+   * Since: 0.11.UNRELEASED
+   */
+  param_spec = g_param_spec_boxed ("channels", "GPtrArray of TpChannel",
+      "The TpChannel to be dispatched",
+      G_TYPE_PTR_ARRAY,
+      G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
+  g_object_class_install_property (object_class, PROP_CHANNELS,
+      param_spec);
+
+  /**
+   * TpChannelDispatchOperation:possible-handlers:
+   *
+   * A #GStrv containing the well known bus names (starting
+   * with TP_CLIENT_BUS_NAME_BASE) of the possible Handlers for
+   * the channels
+   *
+   * Read-only except during construction.
+   *
+   * This is not guaranteed to be set until tp_proxy_prepare_async() has
+   * finished preparing %TP_CHANNEL_DISPATCH_OPERATION_FEATURE_CORE.
+   *
+   * Since: 0.11.UNRELEASED
+   */
+  param_spec = g_param_spec_boxed ("possible-handlers", "Possible handlers",
+      "Possible handlers for the channels",
+      G_TYPE_STRV,
+      G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
+  g_object_class_install_property (object_class, PROP_POSSIBLE_HANDLERS,
+      param_spec);
+
+  /**
+   * TpChannelDispatchOperation:channel-dispatch-operation-properties:
+   *
+   * The immutable D-Bus properties of this ChannelDispatchOperation,
+   * represented by a #GHashTable where the keys are D-Bus
+   * interface name + "." + property name, and the values are #GValue instances.
+   *
+   * Read-only except during construction. If this is not provided
+   * during construction, it is not guaranteed to be set until
+   * tp_proxy_prepare_async() has finished preparing
+   * %TP_CHANNEL_DISPATCH_OPERATION_FEATURE_CORE.
+   *
+   * Since: 0.11.UNRELEASED
+   */
+  param_spec = g_param_spec_boxed ("channel-dispatch-operation-properties",
+      "Immutable D-Bus properties",
+      "A map D-Bus interface + \".\" + property name => GValue",
+      TP_HASH_TYPE_QUALIFIED_PROPERTY_VALUE_MAP,
+      G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY | G_PARAM_STATIC_STRINGS);
+  g_object_class_install_property (object_class,
+      PROP_CHANNEL_DISPATCH_OPERATION_PROPERTIES, param_spec);
 
   proxy_class->interface = TP_IFACE_QUARK_CHANNEL_DISPATCH_OPERATION;
   proxy_class->must_have_unique_name = TRUE;
@@ -220,7 +599,7 @@ tp_channel_dispatch_operation_init_known_interfaces (void)
 TpChannelDispatchOperation *
 tp_channel_dispatch_operation_new (TpDBusDaemon *bus_daemon,
     const gchar *object_path,
-    GHashTable *immutable_properties G_GNUC_UNUSED,
+    GHashTable *immutable_properties,
     GError **error)
 {
   TpChannelDispatchOperation *self;
@@ -243,6 +622,7 @@ tp_channel_dispatch_operation_new (TpDBusDaemon *bus_daemon,
         "dbus-connection", ((TpProxy *) bus_daemon)->dbus_connection,
         "bus-name", unique_name,
         "object-path", object_path,
+        "channel-dispatch-operation-properties", immutable_properties,
         NULL));
 
   g_free (unique_name);
