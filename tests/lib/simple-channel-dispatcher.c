8753c51be6fc7edc5404446b5d7cf0c98f1baaae
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2010-07-01 13:25:15 +0200

    simple-channel-dispatcher: add a trick to make CreateChannel() fail

--- tests/lib/simple-channel-dispatcher.c
+++ tests/lib/simple-channel-dispatcher.c
@@ -69,6 +69,16 @@ tp_tests_simple_channel_dispatcher_create_channel (
   gchar *path;
   TpDBusDaemon *dbus;
 
+  if (tp_asv_get_boolean (request, "CreateChannelFail", NULL))
+    {
+      /* Fail to create the channel */
+      GError error = { TP_ERRORS, TP_ERROR_INVALID_ARGUMENT,
+          "Computer says no" };
+
+      dbus_g_method_return_error (context, &error);
+      return;
+    }
+
   /* create and register ChannelRequest */
   requests = g_ptr_array_sized_new (1);
   g_ptr_array_add (requests, request);
