a5a7a96f55f9b490da50a97df9b4e26f219e0454
Simon McVittie <simon.mcvittie@collabora.co.uk>
2010-07-26 14:47:53 +0100

    Hide TpBaseConnectionCreateHandleReposImpl in a way that satisfies gtk-doc

--- telepathy-glib/base-connection.h
+++ telepathy-glib/base-connection.h
@@ -60,7 +60,9 @@ struct _TpBaseConnectionClass {
     GObjectClass parent_class;
 
 #ifdef __GI_SCANNER__
+    /*<private>*/
     GCallback _internal_create_handle_repos;
+    /*<public>*/
 #else
     TpBaseConnectionCreateHandleReposImpl create_handle_repos;
 #endif
