decd9fd4edff304bf02f6f1c311ec4f4ac740c7e
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2010-09-06 13:02:34 +0200

    offerer: port to TpAccountChannelRequest

--- examples/client/stream-tubes/offerer.c
+++ examples/client/stream-tubes/offerer.c
@@ -50,19 +50,20 @@ _tube_offered (GObject *tube,
   g_debug ("Tube offered");
 }
 
-
 static void
-_channel_prepared (GObject *channel,
-    GAsyncResult *res,
+_channel_created (GObject *source,
+    GAsyncResult *result,
     gpointer user_data)
 {
-  TpStreamTube *tube;
+  TpChannel *channel;
   GError *error = NULL;
+  TpStreamTube *tube;
 
-  tp_proxy_prepare_finish (channel, res, &error);
+  channel = tp_account_channel_request_create_and_handle_channel_finish (
+      TP_ACCOUNT_CHANNEL_REQUEST (source), result, NULL, &error);
   g_assert_no_error (error);
 
-  g_debug ("Channel prepared");
+  g_debug ("Channel created: %s", tp_proxy_get_object_path (channel));
 
   tube = tp_stream_tube_new (TP_CHANNEL (channel));
   /* the TpStreamTube holds the only reference to @channel */
@@ -74,90 +75,6 @@ _channel_prepared (GObject *channel,
   tp_stream_tube_offer_async (tube, NULL, _tube_offered, NULL);
 }
 
-
-static void
-_channel_created (TpConnection *conn,
-    const gchar *channel_path,
-    GHashTable *props,
-    const GError *in_error,
-    gpointer user_data,
-    GObject *weak_obj)
-{
-  TpChannel *channel;
-  GError *error = NULL;
-
-  g_assert_no_error ((GError *) in_error);
-
-  g_debug ("Channel created: %s", channel_path);
-
-  channel = tp_channel_new_from_properties (conn, channel_path, props,
-      &error);
-  g_assert_no_error (error);
-
-  tp_proxy_prepare_async (channel, NULL, _channel_prepared, NULL);
-}
-
-
-static void
-_connection_prepared (GObject *conn,
-    GAsyncResult *res,
-    gpointer user_data)
-{
-  GHashTable *request;
-  GError *error = NULL;
-
-  tp_proxy_prepare_finish (conn, res, &error);
-  g_assert_no_error (error);
-
-  g_debug ("Connection prepared");
-  g_debug ("Requesting channel");
-
-  request = tp_asv_new (
-      TP_PROP_CHANNEL_CHANNEL_TYPE,
-      G_TYPE_STRING,
-      TP_IFACE_CHANNEL_TYPE_STREAM_TUBE,
-
-      TP_PROP_CHANNEL_TARGET_HANDLE_TYPE,
-      G_TYPE_UINT,
-      TP_HANDLE_TYPE_CONTACT,
-
-      TP_PROP_CHANNEL_TARGET_ID,
-      G_TYPE_STRING,
-      user_data,
-
-      TP_PROP_CHANNEL_TYPE_STREAM_TUBE_SERVICE,
-      G_TYPE_STRING,
-      "ExampleService",
-
-      NULL);
-
-  tp_cli_connection_interface_requests_call_create_channel (
-      TP_CONNECTION (conn), -1,
-      request,
-      _channel_created,
-      NULL, NULL, NULL);
-
-  g_hash_table_destroy (request);
-}
-
-
-static void
-_account_prepared (GObject *account,
-    GAsyncResult *res,
-    gpointer user_data)
-{
-  GError *error = NULL;
-
-  tp_proxy_prepare_finish (account, res, &error);
-  g_assert_no_error (error);
-
-  g_debug ("Account prepared");
-
-  tp_proxy_prepare_async (tp_account_get_connection (TP_ACCOUNT (account)),
-        NULL, _connection_prepared, user_data);
-}
-
-
 int
 main (int argc,
     const char **argv)
@@ -166,6 +83,8 @@ main (int argc,
   TpAccount *account;
   char *account_path;
   GError *error = NULL;
+  TpAccountChannelRequest *req;
+  GHashTable *request;
 
   g_assert (argc == 3);
 
@@ -179,13 +98,39 @@ main (int argc,
   g_assert_no_error (error);
   g_free (account_path);
 
-  tp_proxy_prepare_async (account, NULL, _account_prepared,
-      (gpointer) argv[2]);
+  request = tp_asv_new (
+      TP_PROP_CHANNEL_CHANNEL_TYPE,
+      G_TYPE_STRING,
+      TP_IFACE_CHANNEL_TYPE_STREAM_TUBE,
+
+      TP_PROP_CHANNEL_TARGET_HANDLE_TYPE,
+      G_TYPE_UINT,
+      TP_HANDLE_TYPE_CONTACT,
+
+      TP_PROP_CHANNEL_TARGET_ID,
+      G_TYPE_STRING,
+      argv[2],
+
+      TP_PROP_CHANNEL_TYPE_STREAM_TUBE_SERVICE,
+      G_TYPE_STRING,
+      "ExampleService",
+
+      NULL);
+
+  g_debug ("Offer channel to %s", argv[2]);
+
+  req = tp_account_channel_request_new (account, request,
+      TP_USER_ACTION_TIME_CURRENT_TIME);
+
+  tp_account_channel_request_create_and_handle_channel_async (req, NULL,
+      _channel_created, NULL);
 
   loop = g_main_loop_new (NULL, FALSE);
   g_main_loop_run (loop);
 
   g_object_unref (account);
+  g_object_unref (req);
+  g_hash_table_unref (request);
   g_main_loop_unref (loop);
 
   return 0;
