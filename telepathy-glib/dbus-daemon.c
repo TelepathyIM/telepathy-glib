314f4a25b747507036ccd47f583cd5087f3701ef
Simon McVittie <simon.mcvittie@collabora.co.uk>
2010-09-15 15:10:45 +0100

    Remove tp_dbus_daemon_cancel_name_owner_watch from introspection
    After discussion with pwithnall, it looks as though this method isn't
    useful for g-i bindings.

--- telepathy-glib/dbus-daemon.c
+++ telepathy-glib/dbus-daemon.c
@@ -592,10 +592,10 @@ _tp_dbus_daemon_stop_watching (TpDBusDaemon *self,
 }
 
 /**
- * tp_dbus_daemon_cancel_name_owner_watch:
+ * tp_dbus_daemon_cancel_name_owner_watch: (skip)
  * @self: the D-Bus daemon
  * @name: the name that was being watched
- * @callback: (scope call): the callback that was called
+ * @callback: the callback that was called
  * @user_data: the user data that was provided
  *
  * If there was a previous call to tp_dbus_daemon_watch_name_owner()
