a8d0f9b117f376c8371865b8843c5e2ec86a6702
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2010-10-21 12:31:11 +0200

    add TpClientMessage

---
+++ telepathy-glib/client-message.c
@@ -0,0 +1,79 @@
+/*
+ * client-message.c - Source for TpClientMessage
+ * Copyright (C) 2006-2010 Collabora Ltd.
+ * Copyright (C) 2006-2008 Nokia Corporation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/**
+ * SECTION:client-message
+ * @title: TpClientMessage
+ * @short_description: a message in the Telepathy message interface, client side
+ *
+ * #TpClientMessage represent a message send using the Message interface.
+ *
+ * @since 0.13.UNRELEASED
+ */
+
+#include "client-message.h"
+#include "client-message-internal.h"
+#include "message-internal.h"
+
+#include <telepathy-glib/dbus.h>
+#include <telepathy-glib/gtypes.h>
+#include <telepathy-glib/util.h>
+
+/**
+ * TpClientMessage:
+ *
+ * Opaque structure representing a message in the Telepathy messages interface
+ * (client side).
+ */
+
+G_DEFINE_TYPE (TpClientMessage, tp_client_message, TP_TYPE_MESSAGE)
+
+struct _TpClientMessagePrivate
+{
+  gpointer unused;
+};
+
+static void
+tp_client_message_dispose (GObject *object)
+{
+  //TpClientMessage *self = TP_CLIENT_MESSAGE (object);
+  void (*dispose) (GObject *) =
+    G_OBJECT_CLASS (tp_client_message_parent_class)->dispose;
+
+  if (dispose != NULL)
+    dispose (object);
+}
+
+static void
+tp_client_message_class_init (TpClientMessageClass *klass)
+{
+  GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
+
+  gobject_class->dispose = tp_client_message_dispose;
+
+  g_type_class_add_private (gobject_class, sizeof (TpClientMessagePrivate));
+}
+
+static void
+tp_client_message_init (TpClientMessage *self)
+{
+  self->priv = G_TYPE_INSTANCE_GET_PRIVATE ((self), TP_TYPE_MESSAGE,
+      TpClientMessagePrivate);
+}
