c5172489648a7f835e975c1c7b2304b605e71581
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2010-11-04 18:47:57 +0100

    test tp_channel_leave_async() with a group channel

--- tests/dbus/channel.c
+++ tests/dbus/channel.c
@@ -16,6 +16,7 @@
 #include "tests/lib/util.h"
 #include "tests/lib/simple-conn.h"
 #include "tests/lib/textchan-null.h"
+#include "tests/lib/textchan-group.h"
 
 typedef struct {
     GMainLoop *mainloop;
@@ -24,11 +25,14 @@ typedef struct {
     /* Service side objects */
     TpBaseConnection *base_connection;
     TpTestsTextChannelNull *chan_contact_service;
+    TpTestsTextChannelGroup *chan_room_service;
     TpHandleRepoIface *contact_repo;
+    TpHandleRepoIface *room_repo;
 
     /* Client side objects */
     TpConnection *connection;
     TpChannel *channel_contact;
+    TpChannel *channel_room;
 
     GError *error /* initialized where needed */;
     gint wait;
@@ -42,6 +46,7 @@ create_contact_chan (Test *test)
   GHashTable *props;
 
   tp_clear_object (&test->chan_contact_service);
+  tp_clear_object (&test->chan_room_service);
 
   /* Create service-side tube channel object */
   chan_path = g_strdup_printf ("%s/Channel",
@@ -75,6 +80,52 @@ create_contact_chan (Test *test)
 }
 
 static void
+create_room_chan (Test *test)
+{
+  gchar *chan_path;
+  TpHandle handle;
+  GHashTable *props;
+
+  tp_clear_object (&test->chan_room_service);
+
+  /* Create service-side tube channel object */
+  chan_path = g_strdup_printf ("%s/Channel2",
+      tp_proxy_get_object_path (test->connection));
+
+  test->room_repo = tp_base_connection_get_handles (test->base_connection,
+      TP_HANDLE_TYPE_ROOM);
+  g_assert (test->room_repo != NULL);
+
+  handle = tp_handle_ensure (test->room_repo, "test", NULL, &test->error);
+
+  g_assert_no_error (test->error);
+
+  test->chan_room_service = g_object_new (
+      TP_TESTS_TYPE_TEXT_CHANNEL_GROUP,
+      "connection", test->base_connection,
+      "handle", handle,
+      "object-path", chan_path,
+      NULL);
+
+  /* FIXME: This is crack a chan_room_service is actually not a
+   * TpTestsTextChannelNull */
+  props = tp_tests_text_channel_get_props (
+      (TpTestsTextChannelNull *) test->chan_room_service);
+
+  test->channel_room = tp_channel_new_from_properties (test->connection,
+      chan_path, props, &test->error);
+  g_assert_no_error (test->error);
+
+  /* We are in the muc */
+  tp_tests_text_channel_group_join (test->chan_room_service);
+
+  g_free (chan_path);
+
+  tp_handle_unref (test->room_repo, handle);
+  g_hash_table_unref (props);
+}
+
+static void
 setup (Test *test,
        gconstpointer data)
 {
@@ -88,6 +139,7 @@ setup (Test *test,
       "me@test.com", &test->base_connection, &test->connection);
 
   create_contact_chan (test);
+  create_room_chan (test);
 }
 
 static void
@@ -101,6 +153,7 @@ teardown (Test *test,
   test->mainloop = NULL;
 
   tp_clear_object (&test->chan_contact_service);
+  tp_clear_object (&test->chan_room_service);
 
   tp_cli_connection_run_disconnect (test->connection, -1, &test->error, NULL);
   g_assert_no_error (test->error);
@@ -109,6 +162,7 @@ teardown (Test *test,
   g_object_unref (test->base_connection);
 
   tp_clear_object (&test->channel_contact);
+  tp_clear_object (&test->channel_room);
 }
 
 static void
@@ -217,6 +271,106 @@ test_leave_contact_prepared_reason (Test *test,
   g_assert (tp_proxy_get_invalidated (test->channel_contact) != NULL);
 }
 
+/* Room tests */
+static void
+check_not_removed (TpTestsTextChannelGroup *chan)
+{
+  g_assert_cmpuint (chan->removed_handle, ==, 0);
+  g_assert (chan->removed_message == NULL);
+  g_assert_cmpuint (chan->removed_reason, ==, 0);
+}
+
+static void
+check_removed (TpTestsTextChannelGroup *chan)
+{
+  g_assert_cmpuint (chan->removed_handle, !=, 0);
+  g_assert_cmpstr (chan->removed_message, ==, "Bye Bye");
+  g_assert_cmpuint (chan->removed_reason, ==,
+      TP_CHANNEL_GROUP_CHANGE_REASON_BUSY);
+}
+
+static void
+test_leave_room_unprepared_no_reason (Test *test,
+    gconstpointer data G_GNUC_UNUSED)
+{
+  g_assert (tp_proxy_get_invalidated (test->channel_room) == NULL);
+
+  tp_channel_leave_async (test->channel_room,
+      TP_CHANNEL_GROUP_CHANGE_REASON_NONE,
+      NULL, channel_leave_cb, test);
+
+  g_main_loop_run (test->mainloop);
+  g_assert_no_error (test->error);
+
+  g_assert (tp_proxy_get_invalidated (test->channel_room) != NULL);
+  check_not_removed (test->chan_room_service);
+}
+
+static void
+test_leave_room_unprepared_reason (Test *test,
+    gconstpointer data G_GNUC_UNUSED)
+{
+  g_assert (tp_proxy_get_invalidated (test->channel_room) == NULL);
+
+  tp_channel_leave_async (test->channel_room,
+      TP_CHANNEL_GROUP_CHANGE_REASON_BUSY,
+      "Bye Bye", channel_leave_cb, test);
+
+  g_main_loop_run (test->mainloop);
+  g_assert_no_error (test->error);
+
+  g_assert (tp_proxy_get_invalidated (test->channel_room) != NULL);
+  check_removed (test->chan_room_service);
+}
+
+static void
+test_leave_room_prepared_no_reason (Test *test,
+    gconstpointer data G_GNUC_UNUSED)
+{
+  GQuark features[] = { TP_CHANNEL_FEATURE_CORE, 0 };
+
+  g_assert (tp_proxy_get_invalidated (test->channel_room) == NULL);
+
+  tp_proxy_prepare_async (test->channel_room, features,
+      channel_prepared_cb, test);
+  g_main_loop_run (test->mainloop);
+  g_assert_no_error (test->error);
+
+  tp_channel_leave_async (test->channel_room,
+      TP_CHANNEL_GROUP_CHANGE_REASON_NONE,
+      NULL, channel_leave_cb, test);
+
+  g_main_loop_run (test->mainloop);
+  g_assert_no_error (test->error);
+
+  g_assert (tp_proxy_get_invalidated (test->channel_room) != NULL);
+  check_not_removed (test->chan_room_service);
+}
+
+static void
+test_leave_room_prepared_reason (Test *test,
+    gconstpointer data G_GNUC_UNUSED)
+{
+  GQuark features[] = { TP_CHANNEL_FEATURE_CORE, 0 };
+
+  g_assert (tp_proxy_get_invalidated (test->channel_room) == NULL);
+
+  tp_proxy_prepare_async (test->channel_room, features,
+      channel_prepared_cb, test);
+  g_main_loop_run (test->mainloop);
+  g_assert_no_error (test->error);
+
+  tp_channel_leave_async (test->channel_room,
+      TP_CHANNEL_GROUP_CHANGE_REASON_BUSY,
+      "Bye Bye", channel_leave_cb, test);
+
+  g_main_loop_run (test->mainloop);
+  g_assert_no_error (test->error);
+
+  g_assert (tp_proxy_get_invalidated (test->channel_room) != NULL);
+  check_removed (test->chan_room_service);
+}
+
 int
 main (int argc,
       char **argv)
@@ -236,6 +390,15 @@ main (int argc,
       test_leave_contact_prepared_no_reason, teardown);
   g_test_add ("/channel/leave/contact/prepared/reason", Test, NULL, setup,
       test_leave_contact_prepared_reason, teardown);
+
+  g_test_add ("/channel/leave/room/unprepared/no-reason", Test, NULL, setup,
+      test_leave_room_unprepared_no_reason, teardown);
+  g_test_add ("/channel/leave/room/unprepared/reason", Test, NULL, setup,
+      test_leave_room_unprepared_reason, teardown);
+  g_test_add ("/channel/leave/room/prepared/no-reason", Test, NULL, setup,
+      test_leave_room_prepared_no_reason, teardown);
+  g_test_add ("/channel/leave/room/prepared/reason", Test, NULL, setup,
+      test_leave_room_prepared_reason, teardown);
 
   return g_test_run ();
 }
