3c048cdea0a92cff46a2d51e8b34b5b133151ebf
Simon McVittie <simon.mcvittie@collabora.co.uk>
2010-11-17 17:18:22 +0000

    fd.o #31663: use tp_dbus_daemon_register_object to avoid tp_get_bus()
    Reviewed-by: Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>

--- src/telepathy-logger.c
+++ src/telepathy-logger.c
@@ -114,7 +114,7 @@ telepathy_logger_dbus_init (void)
     }
 
   dbus_srv = _tpl_dbus_service_new ();
-  dbus_g_connection_register_g_object (tp_get_bus (), TPL_DBUS_SRV_OBJECT_PATH,
+  tp_dbus_daemon_register_object (tp_bus, TPL_DBUS_SRV_OBJECT_PATH,
       G_OBJECT (dbus_srv));
 
   DEBUG ("TPL DBus service registered to: %s",
