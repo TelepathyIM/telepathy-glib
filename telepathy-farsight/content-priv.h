75644f79eafecc9e3359cb2ea93c6f97cbc4a9e7
Olivier Crête <olivier.crete@collabora.co.uk>
2010-12-30 15:12:58 -0500

    TfContent: Add start/stop-sending signals

--- telepathy-farsight/content-priv.h
+++ telepathy-farsight/content-priv.h
@@ -8,11 +8,16 @@ G_BEGIN_DECLS
 
 struct _TfContent {
   GObject parent;
+
+  gboolean sending;
 };
 
 struct _TfContentClass{
   GObjectClass parent_class;
 };
+
+gboolean _tf_content_start_sending (TfContent *self);
+void _tf_content_stop_sending (TfContent *self);
 
 
 G_END_DECLS
--- telepathy-farsight/content.c
+++ telepathy-farsight/content.c
@@ -5,6 +5,7 @@
 #include <gst/farsight/fs-conference-iface.h>
 
 #include "channel.h"
+#include "tf-signals-marshal.h"
 
 
 G_DEFINE_ABSTRACT_TYPE (TfContent, tf_content, G_TYPE_OBJECT);
@@ -20,8 +21,12 @@ enum
 
 enum
 {
+  SIGNAL_START_SENDING,
+  SIGNAL_STOP_SENDING,
   SIGNAL_COUNT
 };
+
+static guint signals[SIGNAL_COUNT] = {0};
 
 static void
 tf_content_class_init (TfContentClass *klass)
@@ -56,10 +61,84 @@ tf_content_class_init (TfContentClass *klass)
           "Sink GstPad for this content",
           GST_TYPE_PAD,
           G_PARAM_READABLE | G_PARAM_STATIC_STRINGS));
+
+  /**
+   * TfContent::start-sending
+   * @content: the #TfContent
+   * @direction: The direction for which this resource is requested
+   *  (as a #TpMediaDirection
+   *
+   * This signal is emitted when the connection manager ask to send media.
+   * For example, this can be used to open a camera, start recording from a
+   * microphone or play back a file. The application should start
+   * sending data on the #TfContent:sink-pad
+   *
+   * Returns: %TRUE if the application can start providing data or %FALSE
+   * otherwise
+   */
+
+  signals[SIGNAL_START_SENDING] =
+      g_signal_new ("start-sending",
+          G_OBJECT_CLASS_TYPE (klass),
+          G_SIGNAL_RUN_LAST,
+          0,
+          g_signal_accumulator_true_handled, NULL,
+          _tf_marshal_BOOLEAN__VOID,
+          G_TYPE_BOOLEAN, 0);
+
+
+  signals[SIGNAL_STOP_SENDING] =
+      g_signal_new ("stop-sending",
+          G_OBJECT_CLASS_TYPE (klass),
+          G_SIGNAL_RUN_LAST,
+          0, NULL, NULL,
+          g_cclosure_marshal_VOID__VOID,
+          G_TYPE_NONE, 0);
 }
 
 
 static void
 tf_content_init (TfContent *self)
 {
+}
+
+
+gboolean
+_tf_content_start_sending (TfContent *self)
+{
+  GValue instance = {0};
+  GValue sending_success_val = {0,};
+  gboolean sending_success;
+
+  if (self->sending)
+    return TRUE;
+
+  g_value_init (&sending_success_val, G_TYPE_BOOLEAN);
+  g_value_set_boolean (&sending_success_val, TRUE);
+
+  g_value_init (&instance, TF_TYPE_CONTENT);
+  g_value_set_object (&instance, self);
+
+  g_debug ("Requesting that the application start sending");
+
+  g_signal_emitv (&instance, signals[SIGNAL_START_SENDING], 0,
+      &sending_success_val);
+  sending_success = g_value_get_boolean (&sending_success_val);
+
+  g_value_unset (&instance);
+
+  g_debug ("Request to start sending %s",
+      sending_success ? "succeeded" : "failed");
+
+  self->sending = sending_success;
+
+  return sending_success;
+}
+
+void
+_tf_content_stop_sending (TfContent *self)
+{
+  g_signal_emit (self, signals[SIGNAL_STOP_SENDING], 0);
+
+  self->sending = FALSE;
 }
