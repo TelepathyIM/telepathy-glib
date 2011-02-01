40572657bf182cb27e35780d71ffcf57ef864765
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2011-02-01 14:18:46 +0000

    Add global header and global init function
    Because we're using extensions we need to initialize telepathy-farstream
    early on so the various extra D-Bus interfaces are registered before the
    proxies are generated.
    
    Take this opportunity to add a global header, so users only have to
    include one header.

---
+++ telepathy-farstream/telepathy-farstream.h
@@ -0,0 +1,34 @@
+/*
+ * telepathy-farstream.h - Source for TfCallChannel
+ * Copyright (C) 2011 Collabora Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef __TF_FARSTREAM_H_
+#define __TF_FARSTREAM_H_
+
+#include <telepathy-farstream/channel.h>
+#include <telepathy-farstream/content.h>
+#include <telepathy-farstream/stream.h>
+
+G_BEGIN_DECLS
+
+void tf_init (void);
+
+G_END_DECLS
+
+#endif /* __TF_FARSTREAM_H__ */
+
