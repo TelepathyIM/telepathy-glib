a2ea6309a9fe52c7f6963a4c36b4f2a95c526c29
Derek Foreman <derek.foreman@collabora.co.uk>
2011-05-13 15:21:42 -0400

    Support building for Android
    https://bugs.freedesktop.org/show_bug.cgi?id=42447

---
+++ Android.mk
@@ -0,0 +1,42 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+include ../telepathy-glib/telepathy-glib/codegen.am
+
+TELEPATHY_GLIB_BUILT_SOURCES := \
+	telepathy-glib/telepathy-glib.pc \
+	telepathy-glib/telepathy-glib-uninstalled.pc \
+	telepathy-glib/Android.mk
+
+TELEPATHY_GLIB_GENMARSHAL := $(nodist_libtelepathy_glib_internal_la_SOURCES) \
+	$(nodist_geninclude_HEADERS)
+
+telepathy-glib-configure-real:
+	cd $(TELEPATHY_GLIB_TOP) ; \
+	CC="$(CONFIGURE_CC)" \
+	CFLAGS="$(CONFIGURE_CFLAGS)" \
+	LD=$(TARGET_LD) \
+	LDFLAGS="$(CONFIGURE_LDFLAGS)" \
+	CPP=$(CONFIGURE_CPP) \
+	CPPFLAGS="$(CONFIGURE_CPPFLAGS)" \
+	PKG_CONFIG_LIBDIR="$(CONFIGURE_PKG_CONFIG_LIBDIR)" \
+	PKG_CONFIG_TOP_BUILD_DIR=$(PKG_CONFIG_TOP_BUILD_DIR) \
+	$(TELEPATHY_GLIB_TOP)/$(CONFIGURE) --host=arm-linux-androideabi && \
+	for file in $(TELEPATHY_GLIB_BUILT_SOURCES); do \
+		rm -f $$file && \
+		make -C $$(dirname $$file) $$(basename $$file) ; \
+	done && \
+	for file in $(TELEPATHY_GLIB_GENMARSHAL); do \
+		rm -f telepathy-glib/$$file && \
+		make -C telepathy-glib $$file ; \
+	done
+
+telepathy-glib-configure: telepathy-glib-configure-real
+
+.PHONY: telepathy-glib-configure
+
+CONFIGURE_TARGETS += telepathy-glib-configure
+
+#include all the subdirs...
+-include $(TELEPATHY_GLIB_TOP)/telepathy-glib/Android.mk
