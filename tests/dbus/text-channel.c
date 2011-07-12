TelepathyIM/telepathy-glib

commit 6fe82938b9cabf4b0a6271231e97e8e7ada7d8c5

stop using IdentifyMessagesCtx

telepathy-glib/text-channel.c
@@ -581,6 +581,17 @@ got_pending_senders_contact (TpTextChannel *self,
 }
 
 static void
+free_parts_list (GList *parts_list)
+{
+  GList *l;
+
+  for (l = parts_list; l != NULL; l = g_list_next (l))
+    g_boxed_free (TP_ARRAY_TYPE_MESSAGE_PART_LIST, l->data);
+
+  g_list_free (parts_list);
+}
+
+static void
 got_pending_senders_contact_by_handle_cb (TpConnection *connection,
     guint n_contacts,
     TpContact * const *contacts,
@@ -590,8 +601,10 @@ got_pending_senders_contact_by_handle_cb (TpConnection *connection,
     gpointer user_data,
     GObject *weak_object)
 {
-  TpTextChannel *self = (TpTextChannel *) weak_object;
+  GSimpleAsyncResult *result = (GSimpleAsyncResult *) weak_object;
   GList *parts_list = user_data;
+  TpTextChannel *self = TP_TEXT_CHANNEL (g_async_result_get_source_object (
+        G_ASYNC_RESULT (result)));
 
   if (error != NULL)
     {
@@ -609,6 +622,8 @@ got_pending_senders_contact_by_handle_cb (TpConnection *connection,
 out:
   _tp_proxy_set_feature_prepared (TP_PROXY (self),
       TP_TEXT_CHANNEL_FEATURE_INCOMING_MESSAGES, TRUE);
+
+  free_parts_list (parts_list);
 }
 
 static void
@@ -621,8 +636,10 @@ got_pending_senders_contact_by_id_cb (TpConnection *connection,
     gpointer user_data,
     GObject *weak_object)
 {
-  TpTextChannel *self = (TpTextChannel *) weak_object;
+  GSimpleAsyncResult *result = (GSimpleAsyncResult *) weak_object;
   GList *parts_list = user_data;
+  TpTextChannel *self = TP_TEXT_CHANNEL (g_async_result_get_source_object (
+        G_ASYNC_RESULT (result)));
 
   if (error != NULL)
     {
@@ -648,45 +665,8 @@ got_pending_senders_contact_by_id_cb (TpConnection *connection,
 out:
   _tp_proxy_set_feature_prepared (TP_PROXY (self),
       TP_TEXT_CHANNEL_FEATURE_INCOMING_MESSAGES, TRUE);
-}
 
-static void
-free_parts_list (gpointer data)
-{
-  GList *parts_list = data;
-  GList *l;
-
-  for (l = parts_list; l != NULL; l = g_list_next (l))
-    g_boxed_free (TP_ARRAY_TYPE_MESSAGE_PART_LIST, l->data);
-
-  g_list_free (parts_list);
-}
-
-typedef struct
-{
-  GList *parts_list;
-  GSimpleAsyncResult *result;
-} IdentifyMessagesCtx;
-
-/* Take the ref on @parts_list */
-static IdentifyMessagesCtx *
-identify_messages_ctx_new (GList *parts_list,
-    GSimpleAsyncResult *result)
-{
-  IdentifyMessagesCtx *ctx = g_slice_new (IdentifyMessagesCtx);
-
-  ctx->parts_list = parts_list;
-  ctx->result = g_object_ref (result);
-  return ctx;
-}
-
-static void
-identify_messages_free (IdentifyMessagesCtx *ctx)
-{
-  free_parts_list (ctx->parts_list);
-  g_object_unref (ctx->result);
-
-  g_slice_free (IdentifyMessagesCtx, ctx);
+  free_parts_list (parts_list);
 }
 
 /* There is no TP_ARRAY_TYPE_PENDING_TEXT_MESSAGE_LIST_LIST (fdo #32433) */
@@ -774,8 +754,6 @@ get_pending_messages_cb (TpProxy *proxy,
   else
     {
       TpConnection *conn;
-      IdentifyMessagesCtx *ctx = identify_messages_ctx_new (parts_list,
-          result);
 
       parts_list = g_list_reverse (parts_list);
 
@@ -784,13 +762,14 @@ get_pending_messages_cb (TpProxy *proxy,
       DEBUG ("Pending messages may be re-ordered, please fix CM (%s)",
           tp_proxy_get_object_path (conn));
 
+      /* Pass ownership of parts_list to the callback */
       if (sender_ids->len == g_list_length (parts_list))
         {
           /* Use the sender ID rather than the handles */
           tp_connection_get_contacts_by_id (conn, sender_ids->len,
               (const gchar * const *) sender_ids->pdata,
-              0, NULL, got_pending_senders_contact_by_id_cb, ctx,
-              (GDestroyNotify) identify_messages_free, G_OBJECT (self));
+              0, NULL, got_pending_senders_contact_by_id_cb, parts_list,
+              NULL, G_OBJECT (result));
         }
       else
         {
@@ -798,8 +777,8 @@ get_pending_messages_cb (TpProxy *proxy,
 
           tp_connection_get_contacts_by_handle (conn, tmp->len,
               (TpHandle *) tmp->data,
-              0, NULL, got_pending_senders_contact_by_handle_cb, ctx,
-              (GDestroyNotify) identify_messages_free, G_OBJECT (self));
+              0, NULL, got_pending_senders_contact_by_handle_cb, parts_list,
+              NULL, G_OBJECT (result));
 
           g_array_unref (tmp);
         }

commit 8e12523d99a495f562066a124536e1287aeeb1c8

TpTextChannel: fix preparing when message-sender-id is missing
6fe8293 introduced a regression where, if a CM does not include
message-sender-id in received messages, preparing a TpTextChannel for a
proxy which has pending messages will never complete.

As of that commit, the only reference to 'result' is owned by the call
to Get(..., 'PendingMessages'), which is released once
get_pending_messages_cb() returns. So the callback for
tp_connection_get_contacts_by_handle/tp_connection_get_contacts_by_id
never fires, because 'result' is specified as the weak object for the
call: the result dies immediately, and hence the get_contacts operation
is cancelled.

This patch makes the calls to get_contacts_by_* hold a reference to the
result which they pass as a weak object. This is an abuse of the
weak_object field to pass both an object and another pointer to the
callback. Arguably it would be clearer to use a context struct, and my
advocating the removal of exactly such a context struct triggered this
bug…

Fixes: <https://bugs.freedesktop.org/show_bug.cgi?id=39172>

telepathy-glib/text-channel.c
@@ -639,6 +639,7 @@ got_pending_senders_contact_by_handle_cb (TpConnection *connection,
 out:
   _tp_proxy_set_feature_prepared (TP_PROXY (self),
       TP_TEXT_CHANNEL_FEATURE_INCOMING_MESSAGES, TRUE);
+  g_object_unref (result);
 }
 
 static void
@@ -680,6 +681,7 @@ got_pending_senders_contact_by_id_cb (TpConnection *connection,
 out:
   _tp_proxy_set_feature_prepared (TP_PROXY (self),
       TP_TEXT_CHANNEL_FEATURE_INCOMING_MESSAGES, TRUE);
+  g_object_unref (result);
 }
 
 /* There is no TP_ARRAY_TYPE_PENDING_TEXT_MESSAGE_LIST_LIST (fdo #32433) */
@@ -782,7 +784,7 @@ get_pending_messages_cb (TpProxy *proxy,
           tp_connection_get_contacts_by_id (conn, sender_ids->len,
               (const gchar * const *) sender_ids->pdata,
               0, NULL, got_pending_senders_contact_by_id_cb, parts_list,
-              (GDestroyNotify) free_parts_list, G_OBJECT (result));
+              (GDestroyNotify) free_parts_list, g_object_ref (result));
         }
       else
         {
@@ -791,7 +793,7 @@ get_pending_messages_cb (TpProxy *proxy,
           tp_connection_get_contacts_by_handle (conn, tmp->len,
               (TpHandle *) tmp->data,
               0, NULL, got_pending_senders_contact_by_handle_cb, parts_list,
-              (GDestroyNotify) free_parts_list, G_OBJECT (result));
+              (GDestroyNotify) free_parts_list, g_object_ref (result));
 
           g_array_unref (tmp);
         }

tests/dbus/text-channel.c
@@ -789,6 +789,52 @@ test_ack_all_pending_messages (Test *test,
   g_assert_cmpuint (g_list_length (messages), ==, 0);
 }
 
+static void
+test_pending_messages_with_no_sender_id (Test *test,
+    gconstpointer data G_GNUC_UNUSED)
+{
+  GQuark features[] = { TP_TEXT_CHANNEL_FEATURE_INCOMING_MESSAGES, 0 };
+  TpMessage *cm_message;
+  TpMessage *signalled_message;
+  GList *messages;
+  TpContact *sender;
+  gchar *text;
+
+  g_test_bug ("39172");
+
+  /* Deliberately passing sender=0 so we can set message-sender manually; if we set
+   * it here, or using tp_cm_message_set_sender(), message-sender-id will be
+   * filled in, which is exactly what we don't want.
+   */
+  cm_message = tp_cm_message_new_text (test->base_connection, 0,
+      TP_CHANNEL_TEXT_MESSAGE_TYPE_NORMAL, "hi mum");
+  tp_message_set_uint32 (cm_message, 0, "message-sender", test->bob);
+  g_assert_cmpstr (NULL, ==,
+      tp_asv_get_string (tp_message_peek (cm_message, 0), "message-sender-id"));
+  tp_message_mixin_take_received (G_OBJECT (test->chan_service), cm_message);
+
+  test->wait = 1;
+  tp_proxy_prepare_async (test->channel, features,
+      proxy_prepare_cb, test);
+  g_main_loop_run (test->mainloop);
+  g_assert_no_error (test->error);
+
+  messages = tp_text_channel_get_pending_messages (test->channel);
+  g_assert (messages != NULL);
+  g_assert_cmpuint (g_list_length (messages), ==, 1);
+
+  signalled_message = messages->data;
+  sender = tp_signalled_message_get_sender (signalled_message);
+  g_assert (sender != NULL);
+  g_assert_cmpstr (tp_contact_get_identifier (sender), ==, "bob");
+
+  text = tp_message_to_text ((TpMessage *) signalled_message, NULL);
+  g_assert_cmpstr (text, ==, "hi mum");
+  g_free (text);
+
+  g_list_free (messages);
+}
+
 int
 main (int argc,
       char **argv)
@@ -816,6 +862,9 @@ main (int argc,
       test_get_sms_length, teardown);
   g_test_add ("/text-channel/ack-all-pending-messages", Test, NULL, setup,
       test_ack_all_pending_messages, teardown);
+
+  g_test_add ("/text-channel/pending-messages-with-no-sender-id", Test, NULL,
+      setup, test_pending_messages_with_no_sender_id, teardown);
 
   return g_test_run ();
 }