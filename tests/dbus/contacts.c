TelepathyIM/telepathy-glib

commit 22a4438161874e36e27282bd3d24a909f4b16f70

TpContact does not always request avatar
If AVATAR_TOKEN was already prepared and we upgrade to have AVATAR_DATA,
the avatar won't be requested

telepathy-glib/contact.c
@@ -3560,16 +3560,21 @@ contacts_context_queue_features (ContactsContext *context)
 #endif
     }
 
-  if (!contacts_context_supports_iface (context,
+  if ((feature_flags & CONTACT_FEATURE_FLAG_AVATAR_TOKEN) != 0 &&
+      !contacts_context_supports_iface (context,
         TP_IFACE_QUARK_CONNECTION_INTERFACE_AVATARS) &&
       tp_proxy_has_interface_by_id (context->connection,
         TP_IFACE_QUARK_CONNECTION_INTERFACE_AVATARS))
     {
-      if ((feature_flags & CONTACT_FEATURE_FLAG_AVATAR_TOKEN) != 0)
-        g_queue_push_tail (&context->todo, contacts_get_avatar_tokens);
+      g_queue_push_tail (&context->todo, contacts_get_avatar_tokens);
+    }
 
-      if ((feature_flags & CONTACT_FEATURE_FLAG_AVATAR_DATA) != 0)
-        g_queue_push_tail (&context->todo, contacts_get_avatar_data);
+  /* There is no contact attribute for avatar data, always use slow path */
+  if ((feature_flags & CONTACT_FEATURE_FLAG_AVATAR_DATA) != 0 &&
+      tp_proxy_has_interface_by_id (context->connection,
+        TP_IFACE_QUARK_CONNECTION_INTERFACE_AVATARS))
+    {
+      g_queue_push_tail (&context->todo, contacts_get_avatar_data);
     }
 
   if ((feature_flags & CONTACT_FEATURE_FLAG_LOCATION) != 0 &&
@@ -3668,12 +3673,6 @@ tp_contact_set_attributes (TpContact *contact,
           g_object_notify ((GObject *) contact, "alias");
         }
     }
-
-  /* There is no attribute for avatar data. If we want it, let's just
-   * pretend it is ready. If avatar is in cache, that will be true as
-   * soon as the token is set from attributes */
-  if (wanted & CONTACT_FEATURE_FLAG_AVATAR_DATA)
-    contact->priv->has_features |= CONTACT_FEATURE_FLAG_AVATAR_DATA;
 
   if (wanted & CONTACT_FEATURE_FLAG_AVATAR_TOKEN)
     {

commit 6afc5819e15200f7cf0970bad982ea245dca4327

Fix ContactList not having avatars
This is a regression introduced by 22a4438161874e36e27282bd3d24a909f4b16f70,
because ContactList does not go through the slow path.

telepathy-glib/contact.c
@@ -2833,6 +2833,17 @@ out:
 }
 
 static void
+contact_maybe_update_avatar_data (TpContact *self)
+{
+  if ((self->priv->has_features & CONTACT_FEATURE_FLAG_AVATAR_DATA) == 0 &&
+      (self->priv->has_features & CONTACT_FEATURE_FLAG_AVATAR_TOKEN) != 0)
+    {
+      self->priv->has_features |= CONTACT_FEATURE_FLAG_AVATAR_DATA;
+      contact_update_avatar_data (self);
+    }
+}
+
+static void
 contacts_bind_to_avatar_retrieved (TpConnection *connection)
 {
   if (!connection->priv->tracking_avatar_retrieved)
@@ -2854,15 +2865,7 @@ contacts_get_avatar_data (ContactsContext *c)
   contacts_bind_to_avatar_retrieved (c->connection);
 
   for (i = 0; i < c->contacts->len; i++)
-    {
-      TpContact *contact = g_ptr_array_index (c->contacts, i);
-
-      if ((contact->priv->has_features & CONTACT_FEATURE_FLAG_AVATAR_DATA) == 0)
-        {
-          contact->priv->has_features |= CONTACT_FEATURE_FLAG_AVATAR_DATA;
-          contact_update_avatar_data (contact);
-        }
-    }
+    contact_maybe_update_avatar_data (g_ptr_array_index (c->contacts, i));
 
   contacts_context_continue (c);
 }
@@ -3679,6 +3682,13 @@ tp_contact_set_attributes (TpContact *contact,
       s = tp_asv_get_string (asv,
           TP_TOKEN_CONNECTION_INTERFACE_AVATARS_TOKEN);
       contact_set_avatar_token (contact, s, TRUE);
+    }
+
+  if (wanted & CONTACT_FEATURE_FLAG_AVATAR_DATA)
+    {
+      /* There is no attribute for the avatar data, this will set the avatar
+       * from cache or start the avatar request if its missing from cache. */
+      contact_maybe_update_avatar_data (contact);
     }
 
   if (wanted & CONTACT_FEATURE_FLAG_PRESENCE)

tests/dbus/contacts.c
@@ -2686,6 +2686,7 @@ test_contact_list (Fixture *f,
   factory = tp_proxy_get_factory (f->client_conn);
   tp_simple_client_factory_add_contact_features_varargs (factory,
       TP_CONTACT_FEATURE_ALIAS,
+      TP_CONTACT_FEATURE_AVATAR_DATA,
       TP_CONTACT_FEATURE_INVALID);
 
   /* Now put it online and wait for contact list state move to success */
@@ -2713,6 +2714,11 @@ test_contact_list (Fixture *f,
   /* Even if we didn't explicitely asked that feature, we should have it for free */
   g_assert (tp_contact_has_feature (contact, TP_CONTACT_FEATURE_SUBSCRIPTION_STATES));
   g_assert_cmpint (tp_contact_get_subscribe_state (contact), ==, TP_SUBSCRIPTION_STATE_ASK);
+  /* We asked for AVATAR_DATA, verify we got it. This is special because it has
+   * no contact attribute, and ContactList preparation does not go through
+   * the slow path. */
+  g_assert (tp_contact_has_feature (contact, TP_CONTACT_FEATURE_AVATAR_DATA));
+
   g_ptr_array_unref (contacts);
 }
 