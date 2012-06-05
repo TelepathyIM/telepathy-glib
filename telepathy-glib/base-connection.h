TelepathyIM/telepathy-glib

commit 1aba6b49919a9ac68671fc49a525e48ccfa75c57

Ignore deprecations in TP_BASE_CONNECTION_ERROR_IF_NOT_CONNECTED
This macro is now implemented using in tp_base_connection_check_connected()
which is annotated with _TP_AVAILABLE_IN_UNRELEASED, but we shouldn't raise an
error if app don't want to use 0.20 API yet.

https://bugs.freedesktop.org/show_bug.cgi?id=50712

telepathy-glib/base-connection.h
@@ -213,7 +213,9 @@ gboolean tp_base_connection_channel_manager_iter_next (
     TpBaseConnection *c_ = (conn); \
     GError *e_ = NULL; \
     \
+    G_GNUC_BEGIN_IGNORE_DEPRECATIONS \
     if (!tp_base_connection_check_connected (c_, &e_)) \
+    G_GNUC_END_IGNORE_DEPRECATIONS \
       { \
         dbus_g_method_return_error ((context), e_); \
         g_error_free (e_); \

commit d0fbd7d4c7434fa7e8ba27695ad924bab9ef5fd8

Fix regression introduced by 1aba6b49919a9ac68671fc49a525e48ccfa75c57
G_GNUC_END_IGNORE_DEPRECATIONS is considered the instruction to run
for the if. So the block following is executed unconditionally.

telepathy-glib/base-connection.h
@@ -219,12 +219,12 @@ gboolean tp_base_connection_channel_manager_iter_next (
     \
     G_GNUC_BEGIN_IGNORE_DEPRECATIONS \
     if (!tp_base_connection_check_connected (c_, &e_)) \
-    G_GNUC_END_IGNORE_DEPRECATIONS \
       { \
         dbus_g_method_return_error ((context), e_); \
         g_error_free (e_); \
         return; \
       } \
+    G_GNUC_END_IGNORE_DEPRECATIONS \
   } G_STMT_END
 
 TpDBusDaemon *tp_base_connection_get_dbus_daemon (TpBaseConnection *self);