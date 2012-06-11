06dce3780791e67488f74c7970febc2141493b1a
Xavier Claessens <xavier.claessens@collabora.co.uk>
2012-06-11 19:24:36 +0200

    Disable single include for good

--- telepathy-glib/intset.h
+++ telepathy-glib/intset.h
@@ -20,7 +20,7 @@
  *
  */
 
-#if defined (TP_DISABLE_SINGLE_INCLUDE) && !defined (_TP_IN_META_HEADER) && !defined (_TP_COMPILATION)
+#if !defined (_TP_IN_META_HEADER) && !defined (_TP_COMPILATION)
 #error "Only <telepathy-glib/telepathy-glib.h> and <telepathy-glib/telepathy-glib-dbus.h> can be included directly."
 #endif
 
