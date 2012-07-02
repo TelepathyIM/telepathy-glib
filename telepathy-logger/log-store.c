e9d9a4821ab63ab589cbe2a4032eaa5e10ea699d
Debarshi Ray <debarshir@freedesktop.org>
2012-07-02 18:31:12 +0200

    log-store: Add G_TYPE_OBJECT as a prerequisite of TPL_TYPE_LOG_STORE
    Without it you can't have properties of type TPL_TYPE_LOG_STORE.

--- telepathy-logger/log-store.c
+++ telepathy-logger/log-store.c
@@ -58,6 +58,7 @@ _tpl_log_store_get_type (void)
       };
       type = g_type_register_static (G_TYPE_INTERFACE, "TplLogStore",
           &info, 0);
+      g_type_interface_add_prerequisite (type, G_TYPE_OBJECT);
     }
   return type;
 }
