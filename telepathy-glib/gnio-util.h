93dd462e247ee8138b5e5088c72a1f9d6be0d047
Simon McVittie <simon.mcvittie@collabora.co.uk>
2012-09-19 13:28:35 +0100

    tp_g_socket_address_from_g_variant, tp_address_g_variant_from_g_socket_address: add
    https://bugs.freedesktop.org/show_bug.cgi?id=55101

--- telepathy-glib/gnio-util.h
+++ telepathy-glib/gnio-util.h
@@ -41,6 +41,15 @@ GValue *tp_address_variant_from_g_socket_address (GSocketAddress *address,
     TpSocketAddressType *type,
     GError **error) G_GNUC_WARN_UNUSED_RESULT;
 
+_TP_AVAILABLE_IN_UNRELEASED
+GSocketAddress *tp_g_socket_address_from_g_variant (TpSocketAddressType type,
+    GVariant *variant,
+    GError **error) G_GNUC_WARN_UNUSED_RESULT;
+_TP_AVAILABLE_IN_UNRELEASED
+GVariant *tp_address_g_variant_from_g_socket_address (GSocketAddress *address,
+    TpSocketAddressType *type,
+    GError **error) G_GNUC_WARN_UNUSED_RESULT;
+
 gboolean tp_unix_connection_send_credentials_with_byte (
     GSocketConnection *connection,
     guchar byte,
