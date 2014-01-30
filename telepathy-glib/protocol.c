58557fc633c44c261d5e1a8c85438fa12bdf72e6
Simon McVittie <simon.mcvittie@collabora.co.uk>
2014-01-30 20:04:08 +0000

    TpProtocol: fix some suspicious memory management
    It works fine as long as a preallocated GArray doesn't move its memory
    buffer for no reason, but it's good to be obviously correct. Spotted while
    working on the previous commit.
    
    Bug: https://bugs.freedesktop.org/show_bug.cgi?id=71093
    Reviewed-by: Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>

--- telepathy-glib/protocol.c
+++ telepathy-glib/protocol.c
@@ -192,15 +192,15 @@ tp_protocol_params_from_param_specs (const GPtrArray *parameters,
     {
       GValue structure = { 0 };
       GValue *tmp;
-      /* Points to the zeroed entry just after the end of the array
-       * - but we're about to extend the array to make it valid */
-      TpConnectionManagerParam *param = &g_array_index (output,
-          TpConnectionManagerParam, output->len);
+      TpConnectionManagerParam *param;
 
       g_value_init (&structure, TP_STRUCT_TYPE_PARAM_SPEC);
       g_value_set_static_boxed (&structure, g_ptr_array_index (parameters, i));
 
       g_array_set_size (output, output->len + 1);
+      /* point to the new last item */
+      param = &g_array_index (output, TpConnectionManagerParam,
+          output->len - 1);
 
       if (!dbus_g_type_struct_get (&structure,
             0, &param->name,
