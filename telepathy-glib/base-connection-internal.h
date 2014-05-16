cada19aa37bb5da8055af4bd80339d7ceed9dda3
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2014-05-16 10:58:40 +0200

    move _TpBaseConnectionPrivate struct to its .c file
    Fix https://bugs.freedesktop.org/show_bug.cgi?id=78377

--- telepathy-glib/base-connection-internal.h
+++ telepathy-glib/base-connection-internal.h
@@ -30,64 +30,6 @@
 
 G_BEGIN_DECLS
 
-struct _TpBaseConnectionPrivate
-{
-  gchar *bus_name;
-  gchar *object_path;
-
-  TpConnectionStatus status;
-
-  TpHandle self_handle;
-  const gchar *self_id;
-
-  /* Telepathy properties */
-  gchar *protocol;
-
-  /* if TRUE, the object has gone away */
-  gboolean dispose_has_run;
-  /* array of (TpChannelManager *) */
-  GPtrArray *channel_managers;
-  /* array of reffed (TpChannelManagerRequest *) */
-  GPtrArray *channel_requests;
-
-  TpHandleRepoIface *handles[TP_NUM_ENTITY_TYPES];
-
-  /* Created in constructed, this is an array of static strings which
-   * represent the interfaces on this connection.
-   *
-   * Note that this is a GArray of gchar*, not a GPtrArray,
-   * so that we can use GArray's convenient auto-null-termination. */
-  GArray *interfaces;
-
-  /* Array of GDBusMethodInvocation * representing Disconnect calls.
-   * If NULL and we are in a state != DISCONNECTED, then we have not started
-   * shutting down yet.
-   * If NULL and we are in state DISCONNECTED, then we have finished shutting
-   * down.
-   * If not NULL, we are trying to shut down (and must be in state
-   * DISCONNECTED). */
-  GPtrArray *disconnect_requests;
-
-  GDBusConnection *dbus_connection;
-  /* TRUE after constructor() returns */
-  gboolean been_constructed;
-  /* TRUE if on D-Bus */
-  gboolean been_registered;
-  /* TRUE if CONNECTED has been signalled to D-Bus, so it's too late to
-   * manipulate the list of interfaces */
-  gboolean been_connected;
-
-  /* g_strdup (unique name) => owned ClientData struct */
-  GHashTable *clients;
-  /* GQuark iface => number of clients interested */
-  GHashTable *interests;
-
-  gchar *account_path_suffix;
-
-  _TpGDBusConnection *connection_skeleton;
-  _TpGDBusConnectionInterfaceRequests *requests_skeleton;
-};
-
 void _tp_base_connection_set_handle_repo (TpBaseConnection *self,
     TpEntityType entity_type,
     TpHandleRepoIface *handle_repo);
--- telepathy-glib/base-connection.c
+++ telepathy-glib/base-connection.c
@@ -219,6 +219,64 @@
 G_DEFINE_ABSTRACT_TYPE (TpBaseConnection, tp_base_connection,
     G_TYPE_DBUS_OBJECT_SKELETON)
 
+struct _TpBaseConnectionPrivate
+{
+  gchar *bus_name;
+  gchar *object_path;
+
+  TpConnectionStatus status;
+
+  TpHandle self_handle;
+  const gchar *self_id;
+
+  /* Telepathy properties */
+  gchar *protocol;
+
+  /* if TRUE, the object has gone away */
+  gboolean dispose_has_run;
+  /* array of (TpChannelManager *) */
+  GPtrArray *channel_managers;
+  /* array of reffed (TpChannelManagerRequest *) */
+  GPtrArray *channel_requests;
+
+  TpHandleRepoIface *handles[TP_NUM_ENTITY_TYPES];
+
+  /* Created in constructed, this is an array of static strings which
+   * represent the interfaces on this connection.
+   *
+   * Note that this is a GArray of gchar*, not a GPtrArray,
+   * so that we can use GArray's convenient auto-null-termination. */
+  GArray *interfaces;
+
+  /* Array of GDBusMethodInvocation * representing Disconnect calls.
+   * If NULL and we are in a state != DISCONNECTED, then we have not started
+   * shutting down yet.
+   * If NULL and we are in state DISCONNECTED, then we have finished shutting
+   * down.
+   * If not NULL, we are trying to shut down (and must be in state
+   * DISCONNECTED). */
+  GPtrArray *disconnect_requests;
+
+  GDBusConnection *dbus_connection;
+  /* TRUE after constructor() returns */
+  gboolean been_constructed;
+  /* TRUE if on D-Bus */
+  gboolean been_registered;
+  /* TRUE if CONNECTED has been signalled to D-Bus, so it's too late to
+   * manipulate the list of interfaces */
+  gboolean been_connected;
+
+  /* g_strdup (unique name) => owned ClientData struct */
+  GHashTable *clients;
+  /* GQuark iface => number of clients interested */
+  GHashTable *interests;
+
+  gchar *account_path_suffix;
+
+  _TpGDBusConnection *connection_skeleton;
+  _TpGDBusConnectionInterfaceRequests *requests_skeleton;
+};
+
 enum
 {
     PROP_PROTOCOL = 1,
